783c1ceab531bf7fa80050c7da71241336a3a1ec
Christian Rössel <c.roessel@fz-juelich.de>
2009-09-02 13:45:33 +0000

    Empty header files that may become the measurement API to the adapter layer.
    Subversion: revision:60

---
+++ src/measurement/include/SILC_API_Events.h
@@ -0,0 +1,4 @@
+#ifndef SILC_API_EVENTS_H
+#define SILC_API_EVENTS_H
+
+#endif /* SILC_API_EVENTS_H */
---
+++ src/measurement/include/SILC_API_Types.h
@@ -0,0 +1,4 @@
+#ifndef SILC_API_TYPES_H
+#define SILC_API_TYPES_H
+
+#endif /* SILC_API_TYPES_H */
