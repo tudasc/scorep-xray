6d28b7de710c05ad471ee5c6c4e0ae9875a20434
Bert Wesarg <bert.wesarg@tu-dresden.de>
2009-09-18 13:21:25 +0000

    add stub for silc_get_location
    Subversion: revision:82

--- src/measurement/SILC_RuntimeManagement.h
+++ src/measurement/SILC_RuntimeManagement.h
@@ -1,4 +1,11 @@
 #ifndef SILC_RUNTIMEMANAGEMENT_H
 #define SILC_RUNTIMEMANAGEMENT_H
 
+/**
+ * Get the location handle of the caller
+ *
+ * @note could be a function pointer?
+ */
+SILC_API_LocationHandle silc_get_location( void );
+
 #endif /* SILC_RUNTIMEMANAGEMENT_H */
