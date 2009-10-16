4195c94ae16dedabe781ea617ebdd9a3eac82f7d
Christian Rössel <c.roessel@fz-juelich.de>
2009-10-16 09:11:14 +0000

    Added Intern to filenames because doxygen is confused (the buildsystem may also complain)
    Subversion: revision:94

---
+++ src/measurement/SILC_RuntimeManagementIntern.h
@@ -0,0 +1,11 @@
+#ifndef SILC_CORE_RUNTIMEMANAGEMENT_H
+#define SILC_CORE_RUNTIMEMANAGEMENT_H
+
+/**
+ * Get the location handle of the caller
+ *
+ * @note could be a function pointer?
+ */
+SILC_LocationHandle silc_get_location( void );
+
+#endif /* SILC_CORE_RUNTIMEMANAGEMENT_H */
