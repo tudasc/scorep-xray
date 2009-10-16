99d6ea7b0eba37769296d0069704a4bd5e9ea33c
Bert Wesarg <bert.wesarg@tu-dresden.de>
2009-10-16 07:50:19 +0000

    Add cover letter to SILC_Config.
    Subversion: revision:93

--- src/measurement/include/SILC_Config.h
+++ src/measurement/include/SILC_Config.h
@@ -2,11 +2,35 @@
 #define SILC_CONFIG_H
 
 /**
+ * @file        SILC_Config.h
+ * @maintainer  Bert Wesarg <Bert.Wesarg@tu-dresden.de>
+ *
+ * @brief Runtime configuration subsystem.
+ *
+ * To centralize the reading and parsing of the configuration the adapters
+ * need to announce their configuration options to the measurement system.
+ *
+ * The configurarion will, beside environment variables, also be read from
+ * configuration files. All these sources will have a defined priority. With
+ * the environment variables as the highest one (this may change, if we can
+ * pass paramters from the tool wrapper to the measurement system, but
+ * the tool could also pass the parameters per environment variables to the
+ * measurement system).
+ *
+ * With this centralizing the measurement system has also the ability to write
+ * the configuration of the measurement into the experiment directory.
+ *
+ * The tools can also provide a way to show all known configuration options
+ * with their current and default value and a appropriate help description to
+ * the user.
+ */
+
+/**
  * Register a set of configure variables to the measurement system.
  *
  * @param variables         array of type SILC_ConfigVariable which will be
  *                          registered to the measurement system
- * @param numberOfVariables number of variables in the @a variables array
+ * @param numberOfVariables number of variables in the \a variables array
  *
  * Example:
  *      SILC_Bool unify;
@@ -29,20 +53,12 @@
  *       system after the call. But most of the members of the variables need
  *       to be valid after the call.
  *       These are:
- *        * @a SILC_ConfigVariable::nameSpace
- *          (questionable)
- *        * @a SILC_ConfigVariable::name
- *          (questionable)
  *        * @a SILC_ConfigVariable::variableReference
- *          (obvious)
+ *          (reason: obvious)
  *        * @a SILC_ConfigVariable::variableContext
- *          (obvious)
+ *          (reason: obvious)
  *        * @a SILC_ConfigVariable::defaultValue
- *          (for resetting to the default value)
- *        * @a SILC_ConfigVariable::shortHelp
- *          (questionable)
- *        * @a SILC_ConfigVariable::longHelp
- *          (questionable)
+ *          (reason: for resetting to the default value)
  *
  * @return Successful registration or failure
  */
@@ -51,6 +67,7 @@ SILC_ErroCode SILC_ConfigRegister( SILC_ConfigVariable* variables,
 
 /**
  * @ToDo how can the online measurement system change configs?
+ * This interface will be private to the measurement system.
  */
 
 #endif /* SILC_CONFIG_H */
