5d30ea64902b515c695cf671fa5efbc45bed8196
Bert Wesarg <bert.wesarg@tu-dresden.de>
2009-10-30 07:24:38 +0000

    fix and clearify control-flow
    Subversion: common:revision:13

--- install-pre-commit.sh
+++ install-pre-commit.sh
@@ -13,14 +13,17 @@ SVNLOOK=/usr/bin/svnlook
 
 for repo in $repos
 do
-    if [ -d /svn-base/$repo-root/hooks ]; then
+    if [ ! -d /svn-base/$repo-root/hooks ]; then
         echo >&2 "No repository for $repo."
         continue
     fi
 
-    $SVNLOOK cat /svn-base/common-root trunk/beautifier/pre-commit > \
-        /svn-base/$repo-root/hooks/pre-commit && \
-        chmod 0755 /svn-base/$repo-root/hooks/pre-commit && \
-        echo "pre-commit hook installed for repository $repo." || \
+
+    if $SVNLOOK cat /svn-base/common-root trunk/beautifier/pre-commit > \
+            /svn-base/$repo-root/hooks/pre-commit && \
+       chmod 0755 /svn-base/$repo-root/hooks/pre-commit; then
+        echo "pre-commit hook installed for repository $repo."
+    else
         echo >&2 "Installation failed for repository $repo."
+    fi
 done
