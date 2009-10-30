0d642e95da99dc86afc6f4cfd0f449be3538f20d
Daniel Lorenz <lorenz@cs.tu-darmstadt.de>
2009-10-30 12:59:56 +0000

    eplaced own initialization definitions by measuremnt definitions.
    Subversion: revision:158

--- src/adapters/mpi/SILC_Mpi_Init.c
+++ src/adapters/mpi/SILC_Mpi_Init.c
@@ -6,32 +6,62 @@
  */
 
 #include "SILC_Mpi_Init.h"
-#include "SILC_Mpi_Reg.h"
 
-const SILC_Adapter_InitFunctions SILC_Mpi_InitFunctions =
+#include <stdio.h>
+
+const SILC_Adapter SILC_Mpi_Adapter =
 {
+    SILC_ADAPTER_MPI,
+    "MPI",
     &SILC_Mpi_Register,
     &SILC_Mpi_InitAdapter,
-    &SILC_Mpi_InitLocation
+    &SILC_Mpi_InitLocation,
+    &SILC_Mpi_FinalLocation,
+    &SILC_Mpi_Finalize,
+    &SILC_Mpi_Deregister
 };
 
-void
+SILC_Error_Code
 SILC_Mpi_Register
     ()
 {
     printf( "In SILC_Mpi_Register\n" );
+    return SILC_SUCCESS;
 }
 
-void
+SILC_Error_Code
 SILC_Mpi_InitAdapter
     ()
 {
     printf( "In SILC_Mpi_InitAdapter\n" );
+    return SILC_SUCCESS;
 }
 
-void
+SILC_Error_Code
 SILC_Mpi_InitLocation
     ()
 {
     printf( "In SILC_Mpi_InitAdapter\n" );
+    return SILC_SUCCESS;
+}
+
+void
+SILC_Mpi_FinalLocation
+    ()
+{
+    printf( "In SILC_Mpi_FinalAdapter\n" );
+}
+
+void
+SILC_Mpi_Finalize
+    ()
+{
+    printf( "In SILC_Mpi_Finalize\n" );
+}
+
+void
+SILC_Mpi_Deregister
+    ()
+{
+    printf( "In SILC_Mpi_Deregister\n" );
 }
--- src/adapters/mpi/SILC_Mpi_Init.h
+++ src/adapters/mpi/SILC_Mpi_Init.h
@@ -3,32 +3,44 @@
     @email d.lorenz@fz-juelich.de
 
     Contains the initialization function definitions for the measurement system.
-
-    The measurement system calls three adapter functions during initializtaion. First,
-    SILC_Mpi_Register is called, which should retrieve the configuration from the
-    measurement system. Second, SILC_Mpi_InitAdapter is called, which should initialize
-    the adapter itself. Finally, SILC_Mpi_InitLocation is called.
  */
 
 #ifndef SILC_MPI_INIT_H
 #define SILC_MPI_INIT_H
 
+#include "SILC_Adapter.h"
+
 /** The register function */
-void
+SILC_Error_Code
 SILC_Mpi_Register
     ();
 
 /** The initialization function */
-void
+SILC_Error_Code
 SILC_Mpi_InitAdapter
     ();
 
 /** The intialization of location functions */
-void
+SILC_Error_Code
 SILC_Mpi_InitLocation
     ();
 
+/** The finalization of location specifics */
+void
+SILC_Mpi_FinalLocation
+    ();
+
+/** Finalization of the MPI adapter */
+void
+SILC_Mpi_Finalize
+    ();
+
+/** Deregistration of MPI adapter */
+void
+SILC_Mpi_Deregister
+    ();
+
 /** The struct which contain the init functions */
-extern const SILC_Adapter_InitFunctions SILC_Mpi_InitFunctions;
+extern const SILC_Adapter SILC_Mpi_InitFunctions;
 
 #endif // SILC_MPI_INIT_H
