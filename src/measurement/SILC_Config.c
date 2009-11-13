79f95d79d4554939dc336cbcf669eba94c735452
Bert Wesarg <bert.wesarg@tu-dresden.de>
2009-11-13 11:43:13 +0000

    Dump value of environmental variables in ConfigRegister()
    The current format of the environmental variables is as follow:
    
    with namespace:
        SILC_<namespace>_<name>
    
    without namespace:
        SILC_<name>
    
    No case conversion is performed, the call specified the case.
    
    Subversion: revision:207

--- src/measurement/SILC_Config.c
+++ src/measurement/SILC_Config.c
@@ -13,6 +13,7 @@
 
 #include <SILC_Config.h>
 
+#include "silc_types.h"
 
 SILC_Error_Code
 SILC_ConfigRegister
@@ -25,12 +26,34 @@ SILC_ConfigRegister
              __func__, numberOfVariables );
     for ( uint32_t i = 0; i < numberOfVariables; ++i )
     {
+        /* "SILC" (+ "_" + namespace)? + "_" + name + 1 */
+        char environment_variable_name[ 7 + 2 * 32 ];
+
         fprintf( stderr, "Variable:      %s/%s\n",
                  variables[ i ].nameSpace, variables[ i ].name );
         fprintf( stderr, "  Type:        %s\n",
                  silc_config_type_to_string( variables[ i ].type ) );
         fprintf( stderr, "  Default:     %s\n", variables[ i ].defaultValue );
         fprintf( stderr, "  Description: %s\n", variables[ i ].shortHelp );
+
+        sprintf( environment_variable_name, "SILC%s%.32s_%.32s",
+                 variables[ i ].nameSpace ? "_" : "",
+                 variables[ i ].nameSpace ? variables[ i ].nameSpace : "",
+                 variables[ i ].name );
+
+        fprintf( stderr, "  Environmental Name: %s\n",
+                 environment_variable_name );
+
+        const char* environment_variable_value =
+            getenv( environment_variable_name );
+        if ( environment_variable_value )
+        {
+            fprintf( stderr, "    Value: %s\n", environment_variable_value );
+        }
+        else
+        {
+            fprintf( stderr, "    Variable is unset\n" );
+        }
     }
 
     return SILC_SUCCESS;
