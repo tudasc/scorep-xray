221da19b1dc2e2b1308d2161de0ad410af1e527a
René Jäkel <rene.jaekel@tu-dresden.de>
2009-12-07 17:57:54 +0000

    shift declaration to compiler source file
    Subversion: revision:364

--- src/adapters/include/SILC_Compiler_Init.h
+++ src/adapters/include/SILC_Compiler_Init.h
@@ -22,24 +22,9 @@
  * @brief Compiler adapter interface support to the measurement system
  */
 
-#include <stdio.h>
-
 #include "SILC_Adapter.h"
 
-
-/**
- * @brief Hash table to map function addresses to region identifier
- * identifier is called region handle
- *
- * @param id          hash key (address of function)
- * @param name        associated function name
- * @param fname       file name
- * @param lnobegin    line number of begin of function
- * @param lnoend      line number of end of function
- * @param reghandle   region identifier
- * @param HN          pointer to next element
- */
-typedef struct HashNode
+struct HashNode
 {
     long              id;
     const char*       name;
@@ -51,78 +36,7 @@ typedef struct HashNode
 } HashNode;
 
 
-
-extern void
-silc_compiler_register_region
-(
-    HashNode* hn
-);
-
-
-SILC_Error_Code
-silc_compiler_register
-(
-)
-{
-    printf( " register compiler adapter! \n" );
-    return SILC_SUCCESS;
-}
-
-SILC_Error_Code
-silc_compiler_init_adapter
-    ()
-{
-    printf( " init compiler adapter! \n" );
-    return SILC_SUCCESS;
-}
-
-SILC_Error_Code
-silc_compiler_init_location
-(
-)
-{
-    printf( " compiler adapter init loacation! \n" );
-    return SILC_SUCCESS;
-}
-
-void
-silc_compiler_final_location
-    ()
-{
-    printf( " compiler adapter final loacation! \n" );
-}
-
-void
-silc_compiler_finalize
-    ()
-{
-    printf( " compiler adapter finalize! \n" );
-}
-
-void
-silc_compiler_deregister
-    ()
-{
-    printf( " compiler adapter deregister! \n" );
-}
-
-
-
-/**
- * Initialize the adapter structure for compilers
- */
-const SILC_Adapter SILC_Compiler_Adapter =
-{
-    SILC_ADAPTER_COMPILER,
-    "COMPILER",
-    &silc_compiler_register,
-    &silc_compiler_init_adapter,
-    &silc_compiler_init_location,
-    &silc_compiler_final_location,
-    &silc_compiler_finalize,
-    &silc_compiler_deregister
-};
-
+extern const SILC_Adapter SILC_Compiler_Adapter;
 
 
 /**
@@ -130,6 +44,7 @@ const SILC_Adapter SILC_Compiler_Adapter =
  */
 extern void
 ( *silc_comp_finalize )
+
 (
     void
 );
