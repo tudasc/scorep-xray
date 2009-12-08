15d3498e7408c1f15d5864eb9c7bfc0b2c2ff6bd
Bert Wesarg <bert.wesarg@tu-dresden.de>
2009-12-08 09:37:26 +0000

    Make the test case a little bit more challenging.
    Subversion: revision:367

--- test/hello_functions.c
+++ test/hello_functions.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
 
 int
-foo_quadrat( int number )
-{
-    printf( " in function foo_quadrat \n" );
-    return number * number;
-}
-
-int
 foo_area( int length,
           int width )
 {
@@ -16,22 +9,34 @@ foo_area( int length,
 }
 
 int
+foo_quadrat( int number )
+{
+    printf( " in function foo_quadrat \n" );
+    return foo_area( number, number );
+}
+
+int
 foo_volume( int length,
             int width,
             int height )
 {
     printf( " in function foo_volume \n" );
-    return length * width * height;
+    return foo_area( length, foo_area( width, height ) );
 }
 
-
-main( int argc, char* argv[] )
+int
+main( int   argc,
+      char* argv[] )
 {
+    //MPI_Init( &argc, &argv );
+
     printf( "hello, world\n" );
 
     printf( " the quadrat of 5       : %i \n", foo_quadrat( 5 ) );
     printf( " the area of 5 x 4      : %i \n", foo_area( 5, 4 ) );
     printf( " the volume of 5 x 4 x 3: %i \n", foo_volume( 5, 4, 3 ) );
+
+    //MPI_Finalize();
 
     return 0;
 }
