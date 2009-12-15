fa367d8d958c2e1200746e23b91908227627c333
Daniel Lorenz <lorenz@cs.tu-darmstadt.de>
2009-12-15 14:45:36 +0000

    Fixes for ticket ticket:23
    Subversion: revision:408

--- src/adapters/mpi/SILC_Mpi_Init.c
+++ src/adapters/mpi/SILC_Mpi_Init.c
@@ -73,7 +73,7 @@ SILC_ConfigVariable silc_mpi_configs[] = {
         "enable_groups",
         SILC_CONFIG_TYPE_BITSET,
         &silc_mpi_enabled,
-        silc_mpi_enable_groups,
+        ( void* )silc_mpi_enable_groups,
         "DEFAULT",
         "The names of the function groups which are measured.",
         "The names of the function groups which are measured.\nOther functions are not measured.\nPossible groups are:\n All: All MPI functions\n CG: Communicator and group management\n COLL: Collective functions\n DEFAULT: Default configuration\n ENV: Environmental management\n ERR: MPI Error handling\n EXT: External interface functions\n IO: MPI file I/O\n MISC: Miscellaneous\n P2P: Peer-to-peer communication\n RMA: One sided communication\n SPAWN: Process management\n TOPO: Topology\n TYPE: MPI datatype functions\n",
