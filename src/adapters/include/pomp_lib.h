c0248ad1bf28318f1c05002ae3222183038b48d9
Daniel Lorenz <lorenz@cs.tu-darmstadt.de>
2010-01-11 13:30:48 +0000

    Added docu.
    Subversion: revision:419

--- src/adapters/include/pomp_lib.h
+++ src/adapters/include/pomp_lib.h
@@ -30,6 +30,14 @@
 
 #include "SILC_Adapter.h"
 
+/**
+ * @defgroup POMP POMP Adapter implementation
+ * This module implements the adapter functions fo the POMP adapter. POMP defines a set
+ * of functions inserted by the source-to-source instrumenter OPARI. OPARI instruments
+ * OpenMP construct.
+ * @{
+ */
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -62,98 +70,252 @@ POMP_Init_regions();
 
 /*@}*/
 
-extern void
-POMP_Finalize();
+/** Initializes the POMP adapter. It is inserted at the #pragma pomp inst begin.
+ */
 extern void
 POMP_Init();
+
+/** Finalizes the POMP adapter. It is inserted at the #pragma pomp inst end.
+ */
+extern void
+POMP_Finalize();
+
+/** Disables the POMP adapter.
+ */
 extern void
 POMP_Off();
+
+/** Enables the POMP adapter.
+ */
 extern void
 POMP_On();
+
+/** Called at the begin of a user defined POMP region.
+    @param pomp_handle  The handle of the started region.
+ */
 extern void
 POMP_Begin( POMP_Region_handle pomp_handle );
+
+/** Called at the end of a user defined POMP region.
+    @param pomp_handle  The handle of the ended region.
+ */
 extern void
 POMP_End( POMP_Region_handle pomp_handle );
+
+/** Registers a POMP region and returns a region handle.
+    @param pomp_handle  Returns the handle for the newly registered region.
+    @param ctc_string   A string containing the region data.
+ */
 extern void
 POMP_Assign_handle( POMP_Region_handle* pomp_handle,
                     const char          ctc_string[] );
 
 #ifdef _OPENMP
+/** Called before an atomic statement.
+    @param pomp_handle  The handle of the started region.
+ */
 extern void
 POMP_Atomic_enter( POMP_Region_handle pomp_handle );
+
+/** Called after an atomic statement.
+    @param pomp_handle  The handle of the ended region.
+ */
 extern void
 POMP_Atomic_exit( POMP_Region_handle pomp_handle );
+
+/** Called before an barrier.
+    @param pomp_handle  The handle of the started region.
+ */
 extern void
 POMP_Barrier_enter( POMP_Region_handle pomp_handle );
+
+/** Called after an barrier.
+    @param pomp_handle  The handle of the ended region.
+ */
 extern void
 POMP_Barrier_exit( POMP_Region_handle pomp_handle );
+
+/** Called before an flush.
+    @param pomp_handle  The handle of the started region.
+ */
 extern void
 POMP_Flush_enter( POMP_Region_handle pomp_handle );
+
+/** Called after an flush.
+    @param pomp_handle  The handle of the ended region.
+ */
 extern void
 POMP_Flush_exit( POMP_Region_handle pomp_handle );
+
+/** Called at the start of a critical region.
+    @param pomp_handle  The handle of the started region.
+ */
 extern void
 POMP_Critical_begin( POMP_Region_handle pomp_handle );
+
+/** Called at the end of a critical region.
+    @param pomp_handle  The handle of the ended region.
+ */
 extern void
 POMP_Critical_end( POMP_Region_handle pomp_handle );
+
+/** Called before a critical region.
+    @param pomp_handle  The handle of the started region.
+ */
 extern void
 POMP_Critical_enter( POMP_Region_handle pomp_handle );
+
+/** Called after a critical region.
+    @param pomp_handle  The handle of the region.
+ */
 extern void
 POMP_Critical_exit( POMP_Region_handle pomp_handle );
+
+/** Called before a for loop.
+    @param pomp_handle  The handle of the region.
+ */
 extern void
 POMP_For_enter( POMP_Region_handle pomp_handle );
+
+/** Called after a for loop.
+    @param pomp_handle  The handle of the region.
+ */
 extern void
 POMP_For_exit( POMP_Region_handle pomp_handle );
+
+/** Called at the start of a master region.
+    @param pomp_handle  The handle of the region.
+ */
 extern void
 POMP_Master_begin( POMP_Region_handle pomp_handle );
+
+/** Called at the end of a master region.
+    @param pomp_handle  The handle of the ended region.
+ */
 extern void
 POMP_Master_end( POMP_Region_handle pomp_handle );
+
+/** Called at the start of a parallel region.
+    @param pomp_handle  The handle of the region.
+ */
 extern void
 POMP_Parallel_begin( POMP_Region_handle pomp_handle );
+
+/** Called at the end of a parallel region.
+    @param pomp_handle  The handle of the region.
+ */
 extern void
 POMP_Parallel_end( POMP_Region_handle pomp_handle );
+
+/** Called before a parallel region.
+    @param pomp_handle  The handle of the region.
+ */
 extern void
 POMP_Parallel_fork( POMP_Region_handle pomp_handle );
+
+/** Called after a parallel region.
+    @param pomp_handle  The handle of the region.
+ */
 extern void
 POMP_Parallel_join( POMP_Region_handle pomp_handle );
+
+/** Called at the start of a section.
+    @param pomp_handle  The handle of the region.
+ */
 extern void
 POMP_Section_begin( POMP_Region_handle pomp_handle );
+
+/** Called at the end of a section.
+    @param pomp_handle  The handle of the region.
+ */
 extern void
 POMP_Section_end( POMP_Region_handle pomp_handle );
+
+/** Called before a set of sections.
+    @param pomp_handle  The handle of the region.
+ */
 extern void
 POMP_Sections_enter( POMP_Region_handle pomp_handle );
+
+/** Called after a set of sections.
+    @param pomp_handle  The handle of the region.
+ */
 extern void
 POMP_Sections_exit( POMP_Region_handle pomp_handle );
+
+/** Called at the start of a single region.
+    @param pomp_handle  The handle of the region.
+ */
 extern void
 POMP_Single_begin( POMP_Region_handle pomp_handle );
+
+/** Called at the end of a single region.
+    @param pomp_handle  The handle of the region.
+ */
 extern void
 POMP_Single_end( POMP_Region_handle pomp_handle );
+
+/** Called before a single region.
+    @param pomp_handle  The handle of the region.
+ */
 extern void
 POMP_Single_enter( POMP_Region_handle pomp_handle );
+
+/** Called after a single region.
+    @param pomp_handle  The handle of the region.
+ */
 extern void
 POMP_Single_exit( POMP_Region_handle pomp_handle );
+
+/** Called before a workshare region.
+    @param pomp_handle  The handle of the region.
+ */
 extern void
 POMP_Workshare_enter( POMP_Region_handle pomp_handle );
+
+/** Called after a workshare region.
+    @param pomp_handle  The handle of the region.
+ */
 extern void
 POMP_Workshare_exit( POMP_Region_handle pomp_handle );
 
+/** Wraps the omp_init_lock function */
 extern void
 POMP_Init_lock( omp_lock_t* s );
+
+/** Wraps the omp_destroy_lock function */
 extern void
 POMP_Destroy_lock( omp_lock_t* s );
+
+/** Wraps the omp_set_lock function */
 extern void
 POMP_Set_lock( omp_lock_t* s );
+
+/** Wraps the omp_unset_lock function */
 extern void
 POMP_Unset_lock( omp_lock_t* s );
+
+/** Wraps the omp_test_lock function */
 extern int
 POMP_Test_lock( omp_lock_t* s );
+
+/** Wraps the omp_init_nest_lock function */
 extern void
 POMP_Init_nest_lock( omp_nest_lock_t* s );
+
+/** Wraps the omp_destroy_nest_lock function */
 extern void
 POMP_Destroy_nest_lock( omp_nest_lock_t* s );
+
+/** Wraps the omp_set_nest_lock function */
 extern void
 POMP_Set_nest_lock( omp_nest_lock_t* s );
+
+/** Wraps the omp_unset_nest_lock function */
 extern void
 POMP_Unset_nest_lock( omp_nest_lock_t* s );
+
+/** Wraps the omp_test_nest_lock function */
 extern int
 POMP_Test_nest_lock( omp_nest_lock_t* s );
 
@@ -162,5 +324,5 @@ POMP_Test_nest_lock( omp_nest_lock_t* s );
 #ifdef __cplusplus
 }
 #endif
-
+/** @} */
 #endif
