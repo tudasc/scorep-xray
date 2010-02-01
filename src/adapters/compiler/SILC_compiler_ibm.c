d08a17bea6b091fadd7f57f827564092dd5701a7
René Jäkel <rene.jaekel@tu-dresden.de>
2010-02-01 15:26:24 +0000

    - provide generic compiler data structure for IBM compiler adapter version - connection to measurement system enabled
    Subversion: revision:436

--- src/adapters/compiler/SILC_compiler_ibm.c
+++ src/adapters/compiler/SILC_compiler_ibm.c
@@ -25,6 +25,7 @@
 
 #include <stdio.h>
 
+
 #include <SILC_Utils.h>
 #include <SILC_Events.h>
 #include <SILC_Definitions.h>
@@ -37,22 +38,104 @@
 
 static int xl_init = 1;
 
+
 /**
  * data structure to map function name and region identifier
+ */
+/*
+   typedef struct HashNode {
+   long id;
+   const char* name;
+   const char* fname;
+   int lnobegin;
+   int lnoend;
+   SILC_LineNo       lnobegin;
+   SILC_LineNo       lnoend;
+   int reghandle;
+   SILC_RegionHandle reghandle;
+   struct HashNode* next;
+   } HashNode;
+ */
+
+/*
+ #define HASH_MAX 1021
+ */
+
+/*
+   static HashNode* htab[HASH_MAX];
+ */
+
+/*
+ * Stores region identifier `e' under hash code `h'
+ */
+
+/*
+   void hash_put(
+   long        h,
+   const char* n,
+   const char* fn,
+    int         lno
+   )
+   {
+   long      id  = h % HASH_MAX;
+   HashNode* add = ( HashNode* )malloc( sizeof( HashNode ) );
+   add->id        = h;
+   add->name      = n;
+   add->fname     = fn ? ( const char* )strdup( fn ) : fn;
+   add->lnobegin  = lno;
+
+   add->lnoend    = lno;
+
+   add->reghandle = 1;
+   add->next      = htab[ id ];
+   htab[ id ]     = add;
+   }
+ */
+
+/*
+ * Lookup hash code `h'
+ * Returns pointer to function data if already stored, otherwise 0
+ */
+
+/*
+   static HashNode *hash_get(long h) {
+   long id = h % HASH_MAX;
+   HashNode *curr = htab[id];
+   while ( curr ) {
+   if ( curr->id == h ) {
+     return curr;
+   }
+   curr = curr->next;
+   }
+   return 0;
+   }
  */
 
 
 /*
  * register new region
  */
-void
-silc_xl_register_region
-(
-)
-{
-    SILC_DEBUG_PRINTF( SILC_DEBUG_COMPILER, " register a region: \n" );
-}
+/*
+   HashNode*
+   silc_xl_register_region
+   (
+   HashNode* hn
+   )
+   {
+   printf( " register a region: %i \n", hn->reghandle );
 
+
+    hn->reghandle =  SILC_DefineRegion( hn->name,
+                                        SILC_INVALID_SOURCE_FILE,
+                                        SILC_INVALID_LINE_NO,
+                                        SILC_INVALID_LINE_NO,
+                                        SILC_ADAPTER_COMPILER,
+                                       SILC_REGION_FUNCTION
+                                        );
+
+
+   }
+ */
 
 void
 xl_finalize( void );
@@ -82,7 +165,30 @@ __func_trace_enter( char* name,
                     char* fname,
                     int   lno )
 {
-    SILC_DEBUG_PRINTF( SILC_DEBUG_COMPILER, " function name: %s %s \n", name, fname );
+    HashNode* hn;
+
+    printf( " function name: %s %s \n", name, fname );
+
+    if ( xl_init )
+    {
+        /*      SILC_InitMeasurement();   */
+        xl_finalize();
+    }
+
+    /* put function to list */
+    if ( ( hn = hash_get( ( long )name ) ) == 0 )
+    {
+        hash_put( ( long )name, name, fname, lno );
+        printf( " number %ld and put name -- %s -- to list \n", ( long )name, name );
+    }
+    else
+    {
+        /*      if ( (hn->reghandle == SILC_INVALID_REGION ) ) */
+        if ( ( hn->reghandle == 1 ) )
+        {
+            silc_compiler_register_region( hn );
+        }
+    }
 }
 
 /*
@@ -94,5 +200,11 @@ __func_trace_exit( char* name,
                    char* fname,
                    int   lno )
 {
-    SILC_DEBUG_PRINTF( SILC_DEBUG_COMPILER, "call function %s at exit!!!\n", fname );
+    printf( "call function -- %s -- at exit!!!\n", name );
+    HashNode* hn;
+    /*    SILC_DEBUG_PRINTF( SILC_DEBUG_COMPILER, "call function exit!!!" ); */
+    if ( hn = hash_get( ( long )name ) )
+    {
+        /*    SILC_ExitRegion( hn->reghandle ); */
+    }
 }
