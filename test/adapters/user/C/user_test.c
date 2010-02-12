55654b3ecd5b93570c9a8f89a76b00243b202224
Daniel Lorenz <lorenz@cs.tu-darmstadt.de>
2010-02-12 09:20:44 +0000

    Added test for C user adapter.
    Subversion: revision:460

---
+++ test/adapters/user/C/user_test.c
@@ -0,0 +1,49 @@
+#include "SILC_User.h"
+
+#include "user_test_baz.c.inc"
+
+SILC_USER_METRIC_GROUP_GLOBAL( globalGroup )
+SILC_USER_METRIC_GLOBAL( globalMetric )
+
+extern void
+foo();
+
+void
+test()
+{
+    SILC_USER_FUNC_BEGIN
+    SILC_USER_METRIC_INT64( globalMetric,
+                            2 );
+
+    SILC_USER_FUNC_END
+}
+
+int
+main( int   argc,
+      char* argv[] )
+{
+    SILC_USER_METRIC_GROUP_LOCAL( localGroup );
+    SILC_USER_METRIC_LOCAL( localMetric );
+    SILC_USER_FUNC_BEGIN;
+    SILC_USER_METRIC_GROUP_INIT( localGroup, "localGroup" );
+    SILC_USER_METRIC_GROUP_INIT( globalGroup, "globalGroup" );
+    SILC_USER_METRIC_INIT( localMetric, "localMetric", "s", SILC_USER_METRIC_TYPE_DOUBLE,
+                           SILC_USER_METRIC_CONTEXT_GLOBAL, SILC_USER_METRIC_GROUP_DEFAULT );
+    SILC_USER_METRIC_INIT( globalMetric, "globalMetric", "s", SILC_USER_METRIC_TYPE_INT64,
+                           SILC_USER_METRIC_CONTEXT_CALLPATH, localGroup );
+    foo();
+    SILC_USER_METRIC_DOUBLE( localMetric, 3.0 );
+    baz();
+    SILC_USER_METRIC_INT64( globalMetric, 1 );
+    test();
+    foo();
+    SILC_USER_FUNC_END
+    foo();
+
+    SILC_RECORDING_OFF
+    if ( !SILC_RECORDING_IS_ON )
+    {
+        SILC_RECORDING_ON
+        return 0;
+    }
+}
---
+++ test/adapters/user/C/user_test_foo.c
@@ -0,0 +1,13 @@
+#include "SILC_User.h"
+
+SILC_USER_METRIC_GROUP_EXTERNAL( globalGroup )
+SILC_USER_METRIC_EXTERNAL( globalMetric )
+
+void
+foo()
+{
+    SILC_USER_REGION_DEFINE( foo_handle )
+    SILC_USER_REGION_BEGIN( foo_handle, "foo", SILC_USER_REGION_TYPE_COMMON );
+    SILC_USER_METRIC_INT64( globalMetric, 3 );
+    SILC_USER_REGION_END( foo_handle );
+}
