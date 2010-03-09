e2eaaff65b1387947cafad48fbda39eaf5797ccc
Christian Rössel <c.roessel@fz-juelich.de>
2010-03-09 09:05:51 +0000

    Updated tests
    Subversion: revision:482

---
+++ test/measurement/self_contained_silc_definition_locking.c
@@ -0,0 +1,33 @@
+/*
+ * This file is part of the SILC project (http://www.silc.de)
+ *
+ * Copyright (c) 2009-2011,
+ *    RWTH Aachen, Germany
+ *    Gesellschaft fuer numerische Simulation mbH Braunschweig, Germany
+ *    Technische Universitaet Dresden, Germany
+ *    University of Oregon, Eugene USA
+ *    Forschungszentrum Juelich GmbH, Germany
+ *    Technische Universitaet Muenchen, Germany
+ *
+ * See the COPYING file in the package base directory for details.
+ *
+ */
+
+
+/**
+ * @file       self_contained_silc_definition_locking.c
+ * @maintainer Christian R&ouml;ssel <c.roessel@fz-juelich.de>
+ *
+ * @status ALPHA
+ *
+ *
+ */
+
+
+#include <SILC_DefinitionLocking.h>
+
+int
+main()
+{
+    return 0;
+}
