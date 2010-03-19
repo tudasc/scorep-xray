012f73484a7325ba06e9d3276b6a682f5d1e0186
Bert Wesarg <bert.wesarg@tu-dresden.de>
2010-03-19 08:05:55 +0000

    Change paradigm of hooks framework.
    The previous paradigm of the multiplexer hooks extracted each hook from the
    common repository, and than executed them. This ha the major drawback, that
    all used repositories will immediately use new hooks checked into the
    repository, which makes testing new hooks impossible.
    
    The new paradigm checks the hooks.d directories out at install time into the
    hooks/ directory of the repository. So it can be selctive installed and tested.
    
    
    Subversion: common:revision:63

--- hooks/install-hooks.sh
+++ hooks/install-hooks.sh
@@ -9,8 +9,15 @@ if [ $# -gt 0 ]; then
     repos="$*"
 fi
 
-# use absolute path to svnlook
+# use absolute path
+SVN=/usr/bin/svn
 SVNLOOK=/usr/bin/svnlook
+CP=/bin/cp
+RM=/bin/rm
+CHOWN=/bin/chown
+CHMOD=/bin/chmod
+MKTEMP=/bin/mktemp
+FIND=/usr/bin/find
 
 # path to the common repo
 # TODO make this an option to this script and replace places in the
@@ -22,15 +29,14 @@ cat_common()
     $SVNLOOK cat $COMMON_REPOS "$@"
 }
 
-HOOK="$(mktemp)"
+HOOK="$($MKTEMP)"
 trap "rm -f \"$HOOK\"" 0
 
 for hook in $hooks
 do
-
     cat_common trunk/hooks/$hook > "$HOOK" &&
-        chmod 0755 "$HOOK" &&
-        chown apache:svnsilc "$HOOK"
+        $CHMOD 0755 "$HOOK" &&
+        $CHOWN apache:svnsilc "$HOOK"
     if [ $? -ne 0 ]; then
         echo "Can't generate $hook hook." >&2
         exit 1
@@ -43,12 +49,31 @@ do
             continue
         fi
 
-        cp --archive --backup=numbered "$HOOK" /svn-base/$repo-root/hooks/$hook
+        # install multiplexer hook
+        $CP --archive --backup=numbered "$HOOK" /svn-base/$repo-root/hooks/$hook
         if [ $? -eq 0 ]; then
             echo "$hook hook installed for repository $repo."
         else
             echo >&2 "Installation failed for repository $repo."
         fi
-    done
 
+        # checkout/update .d directory
+        if [ -d /svn-base/$repo-root/hooks/$hook.d/.svn ]; then
+            (
+                cd /svn-base/$repo-root/hooks/$hook.d
+                $SVN up
+            )
+        else
+            rm -rf /svn-base/$repo-root/hooks/$hook.d
+            (
+                cd /svn-base/$repo-root/hooks
+                $SVN co file://$COMMON_REPOS/hooks/$hook.d
+            )
+        fi
+
+        # fix owner of hooks.d
+        # don't fix permission, hooks are only active if they have the
+        # executable bit set in the repository
+        $CHOWN apache:svnsilc /svn-base/$repo-root/hooks/$hook.d/[0-9][0-9]*
+    done
 done
