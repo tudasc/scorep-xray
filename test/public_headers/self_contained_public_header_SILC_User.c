dfdfb4b5e61c44abc4e74d01f4af93fd2d32d528
Christian Rössel <c.roessel@fz-juelich.de>
2010-03-25 16:16:15 +0000

    Added tests for self-containdness of public include files.
    Subversion: revision:508

---
+++ test/public_headers/self_contained_public_header_SILC_User.c
@@ -0,0 +1,35 @@
+/*
+ * This file is part of the SILC project (http://www.silc.de)
+ *
+ * Copyright (c) 2009-2011,
+ *    RWTH Aachen, Germany
+ *    Gesellschaft fuer numerische Simulation mbH Braunschweig, Germany
+ *    Technische Universitaet Dresden, Germany
+ *    University of Oregon, Eugene USA
+ *    Forschungszentrum Juelich GmbH, Germany
+ *    Technische Universitaet Muenchen, Germany
+ *
+ * See the COPYING file in the package base directory for details.
+ *
+ */
+
+
+/**
+ * @file       self_contained_public_header_SILC_User.c
+ * @author     Christian R&ouml;ssel <c.roessel@fz-juelich.de>
+ * @maintainer Christian R&ouml;ssel <c.roessel@fz-juelich.de>
+ *
+ * @status ALPHA
+ *
+ *
+ */
+
+
+#include <SILC_User.h>
+
+
+int
+main()
+{
+    return 0;
+}
