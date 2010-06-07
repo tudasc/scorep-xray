8218117a51a05b7ba347403506c57b397e3782c1
Christian Rössel <c.roessel@fz-juelich.de>
2010-06-07 18:59:54 +0000

    Added call to SILC_OnPMPI_Init() to the MPI_Init_thread() wrapper.
    Subversion: revision:707

--- src/adapters/mpi/SILC_Mpi_Env.c
+++ src/adapters/mpi/SILC_Mpi_Env.c
@@ -172,6 +172,8 @@ MPI_Init_thread( int*    argc,
 
     if ( ( PMPI_Finalized( &fflag ) == MPI_SUCCESS ) && ( fflag == 0 ) )
     {
+        SILC_OnPMPI_Init();
+
         /* initialize communicator management and register MPI_COMM_WORLD */
         silc_mpi_comm_init();
 
