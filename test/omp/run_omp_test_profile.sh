67bcaf94adb5cc5631814f7c99fb4ea11744c035
Daniel Lorenz <lorenz@cs.tu-darmstadt.de>
2010-07-16 00:35:54 +0000

    Added a profiling test.
    Subversion: revision:827

---
+++ test/omp/run_omp_test_profile.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+## 
+## This file is part of the SILC project (http://www.silc.de)
+##
+## Copyright (c) 2009-2011, 
+##    RWTH Aachen, Germany
+##    Gesellschaft fuer numerische Simulation mbH Braunschweig, Germany
+##    Technische Universitaet Dresden, Germany
+##    University of Oregon, Eugene, USA
+##    Forschungszentrum Juelich GmbH, Germany
+##    Technische Universitaet Muenchen, Germany
+##
+## See the COPYING file in the package base directory for details.
+##
+
+## file       run_omp_test.sh
+## maintainer Christian Roessel <c.roessel@fz-juelich.de>
+
+
+SILC_ENABLE_PROFILING=true ./omp_test
+if [ $? -ne 0 ]; then
+    rm -rf silc-measurement-tmp
+    exit 1
+fi
+exit 0
