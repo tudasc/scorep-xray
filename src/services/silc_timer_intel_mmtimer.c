923eb301c81d20c4868450bfd0f4626137915d79
Daniel Lorenz <lorenz@cs.tu-darmstadt.de>
2010-07-22 22:17:12 +0000

    Implemented the mmtimer.
    Subversion: revision:850

--- src/services/silc_timer_intel_mmtimer.c
+++ src/services/silc_timer_intel_mmtimer.c
@@ -16,6 +16,7 @@
 /**
  * @file       silc_timer_intel_mmtimer.c
  * @author     Christian R&ouml;ssel <c.roessel@fz-juelich.de>
+ * @author     Daniel Lorenz <d.lorenz@fz-juelich.de>
  * @maintainer Christian R&ouml;ssel <c.roessel@fz-juelich.de>
  *
  * @status ALPHA
@@ -24,10 +25,8 @@
  * intel_mmtimer timer.
  */
 
-
 #include "SILC_Timing.h"
 #include <config.h>
-
 
 #if HAVE( MMTIMER_H )
 #include <mmtimer.h>
@@ -38,36 +37,79 @@
 #else
 #error "You need to include mmtimer.h"
 #endif
+#include <sys/mman.h>
 
-
-#include <assert.h>
+#include <sys/ioctl.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
 
+#include "SILC_Utils.h"
+
+#define MMTIMER_FULLNAME "/dev/mmtimer"
+
+/**
+   Indicates whether the timer is initialized.
+ */
 static bool isInitialized = false;
+
+/**
+   Stores the clock resolution.
+ */
+static unsigned long ticks_per_sec = 0;
+
+/**
+   Pointer to the clock counter storage.
+ */
+static volatile unsigned long* mmdev_timer_addr = NULL;
 
 void
 SILC_Timer_Initialize()
 {
-    assert( false ); // implement me
     if ( isInitialized )
     {
         return;
     }
+
+    int           fd;
+    unsigned long femtosecs_per_tick = 0;
+    int           offset;
+
+    if ( ( fd = open( MMTIMER_FULLNAME, O_RDONLY ) ) == -1 )
+    {
+        SILC_ERROR_POSIX( "Failed to open " MMTIMER_FULLNAME );
+    }
+
+    if ( ( offset = ioctl( fd, MMTIMER_GETOFFSET, 0 ) ) == -ENOSYS )
+    {
+        SILC_ERROR_POSIX( "Cannot get mmtimer offset" );
+    }
+
+    if ( ( mmdev_timer_addr = mmap( 0, sysconf( _SC_PAGESIZE ) /*getpagesize()*/, PROT_READ, MAP_SHARED, fd, 0 ) )
+         == NULL )
+    {
+        SILC_ERROR_POSIX( "Cannot mmap mmtimer" );
+    }
+    mmdev_timer_addr += offset;
+
+    ioctl( fd, MMTIMER_GETFREQ, &ticks_per_sec );
+
+    close( fd );
     isInitialized = true;
 }
-
 
 uint64_t
 SILC_GetClockTicks()
 {
-    assert( false ); // implement me
-    return 0;
+    return *mmdev_timer_addr;
 }
 
 
 uint64_t
 SILC_GetClockResolution()
 {
-    assert( false ); // implement me
-    return 0;
+    return ticks_per_sec;
 }
