651df382d91f4913e5c2a4fad375298479e5fe9d
Christian Rössel <c.roessel@fz-juelich.de>
2010-07-23 21:53:42 +0000

    * Added framework for up-coming TAU adapter. Just fill the files SILC_Tau.h and SILC_Tau.c. They are compiled and linked into all four SILC libraries (serial, omp, mpi, mpi_omp).  * Fixed distcheck bug introduced by OA branch merge.
    Subversion: revision:858

---
+++ include/silc/SILC_Tau.h
@@ -0,0 +1,32 @@
+#ifndef SILC_TAU_H_
+#define SILC_TAU_H_
+
+/*
+ * This file is part of the SILC project (http://www.silc.de)
+ *
+ * Copyright (c) 2009-2011,
+ *    RWTH Aachen, Germany
+ *    Gesellschaft fuer numerische Simulation mbH Braunschweig, Germany
+ *    Technische Universitaet Dresden, Germany
+ *    University of Oregon, Eugene, USA
+ *    Forschungszentrum Juelich GmbH, Germany
+ *    Technische Universitaet Muenchen, Germany
+ *
+ * See the COPYING file in the package base directory for details.
+ *
+ */
+
+
+/**
+ * @file       /silc/include/silc/SILC_Tau.h
+ * @maintainer TAU group (please put in a name and an email address)
+ *
+ * @status alpha
+ *
+ */
+
+
+
+
+
+#endif /* SILC_TAU_H_ */
---
+++ src/adapters/tau/SILC_Tau.c
@@ -0,0 +1,31 @@
+/*
+ * This file is part of the SILC project (http://www.silc.de)
+ *
+ * Copyright (c) 2009-2011,
+ *    RWTH Aachen, Germany
+ *    Gesellschaft fuer numerische Simulation mbH Braunschweig, Germany
+ *    Technische Universitaet Dresden, Germany
+ *    University of Oregon, Eugene, USA
+ *    Forschungszentrum Juelich GmbH, Germany
+ *    Technische Universitaet Muenchen, Germany
+ *
+ * See the COPYING file in the package base directory for details.
+ *
+ */
+
+
+/**
+ * @file       /silc/src/adapters/tau/SILC_Tau.c
+ * @maintainer TAU group (please put in a name and an email address)
+ *
+ * @status alpha
+ *
+ */
+
+
+#include <config.h>
+#include <SILC_Tau.h>
+
+
+/* *INDENT-OFF* */
+/* *INDENT-ON*  */
