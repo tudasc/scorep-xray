41e81c85be7bde5c289c5d338ea764a48d9e483a
Christian Rössel <c.roessel@fz-juelich.de>
2010-08-24 16:55:55 +0000

    Towards a separate MPI configure script. To be continued.
    Subversion: revision:921

--- src/config.h
+++ src/config.h
@@ -5,7 +5,7 @@
  * @file       config.h
  * @author     Christian R&ouml;ssel <c.roessel@fz-juelich.de>
  * @maintainer Christian R&ouml;ssel <c.roessel@fz-juelich.de>
- * @status     ALPHA
+ * @status     alpha
  *
  * @brief      Provide a single config.h that hides the frontend/backend
  *             issues from the developer
@@ -15,13 +15,21 @@
 #if defined CROSS_BUILD
     #if defined FRONTEND_BUILD
         #include <config-frontend.h>
-    #elif defined BACKEND_BUILD
+    #elif defined BACKEND_BUILD_NOMPI
         #include <config-backend.h>
+    #elif defined BACKEND_BUILD_MPI
+        #include <config-backend-mpi.h>
     #else
-        #error "You can not use config.h without defining either FRONTEND_BUILD or BACKEND_BUILD."
+        #error "You can not use config.h without defining either FRONTEND_BUILD, BACKEND_BUILD_NOMPI or BACKEND_BUILD_MPI."
     #endif
 #elif defined NOCROSS_BUILD
-    #include <config-backend.h>
+    #if defined BACKEND_BUILD_NOMPI
+        #include <config-backend.h>
+    #elif defined BACKEND_BUILD_MPI
+        #include <config-backend-mpi.h>
+    #else
+        #error "You can not use config.h without defining either BACKEND_BUILD_NOMPI or BACKEND_BUILD_MPI."
+    #endif
 #else
     #error "You can not use config.h without defining either CROSS_BUILD or NOCROSS_BUILD."
 #endif
