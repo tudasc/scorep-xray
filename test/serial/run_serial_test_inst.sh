314a8eab411b90fd4c7f9d3c05226490824d3c6d
Daniel Lorenz <lorenz@cs.tu-darmstadt.de>
2010-09-10 17:19:25 +0000

    Separated serial test from instrumentation test.
    Subversion: revision:975

--- test/serial/run_serial_test_inst.sh
+++ test/serial/run_serial_test_inst.sh
@@ -15,13 +15,22 @@
 ##
 
 ## file       run_serial_test.sh
-## maintainer Christian Roessel <c.roessel@fz-juelich.de>
+## maintainer Daniel Lorenz <d.lorenz@fz-juelich.de>
 
-
-./serial_test
-if [ $? -ne 0 ]; then
-    rm -rf silc-measurement-tmp
+rm -f config.h serial_inst_test
+make silc-config-tool-local
+. silc_config.dat
+echo "/* Dummy */" > config.h
+./silc --instrument -verbosity=1 $CC -I. -o serial_inst_test $SRC_ROOT/test/serial/serial_test.c
+if [ ! -e serial_inst_test ]; then
+    rm -f config.h
     exit 1
 fi
+./silc --measure -verbosity=1 ./serial_inst_test
+if [ $? -ne 0 ]; then
+    rm -rf silc-measurement-tmp config.h
+    exit 1
+fi
+rm -f config.h serial_inst_test
 
 exit 0
