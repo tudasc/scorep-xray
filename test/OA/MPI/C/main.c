d2a4a7dfe501a207da778604403eb0f4b8f24b2a
Yury Oleynik <oleynik@in.tum.de>
2011-04-19 12:43:55 +0000

    Extended OA tests
    Subversion: revision:1488

--- test/OA/MPI/C/main.c
+++ test/OA/MPI/C/main.c
@@ -22,6 +22,9 @@
 
 #include "SCOREP_User.h"
 
+double test1(double *a, int rank, int size);
+double test2(double *a, int rank, int size);
+double test3(double *a, int rank, int size);
 int
 main( int    argc,
       char** argv )
@@ -37,30 +40,63 @@ main( int    argc,
     if (myrank==0)
         printf("add example started with %d processes\n", np);
 
-    SCOREP_USER_REGION_DEFINE( mainRegion );
-    SCOREP_USER_OA_PHASE_BEGIN( mainRegion );
-    SCOREP_USER_REGION_BEGIN( mainRegion, "mainRegion", SCOREP_USER_REGION_TYPE_COMMON );
 
-    printf("add is inside the phase\n", np);
     for(k=0;k<30;k++)
     {
+        SCOREP_USER_REGION_DEFINE( mainRegion );
+        SCOREP_USER_OA_PHASE_BEGIN( mainRegion, "mainRegion", SCOREP_USER_REGION_TYPE_COMMON);
 
-        for(i=0;i<10000;i++)
-                a[i] = 500*i;
+        printf("add is inside the phase, iteration %d\n", k);
+        s=test1(a,myrank,10000);
+        s=test2(a,myrank,10000);
+        s=test3(a,myrank,10000);
 
-        for(i=0;i<10000;i++)
-                s += a[i];
+        SCOREP_USER_OA_PHASE_END( mainRegion );
 
-        MPI_Barrier(MPI_COMM_WORLD);
     }
-
-    SCOREP_USER_REGION_END( mainRegion );
-    SCOREP_USER_OA_PHASE_END( mainRegion );
 
     if (myrank==0)
         printf("hello %g\n", a[100]);
 
-        MPI_Finalize();
+    MPI_Finalize();
 
     return retVal;
+}
+
+double test1(double *a, int rank, int size)
+{
+	double s=0;
+	int i;
+	for(i=0;i<size;i++)
+			a[i] = 500*i;
+
+	for(i=0;i<size;i++)
+			s += a[i];
+
+	MPI_Barrier(MPI_COMM_WORLD);
+	return s;
+}
+
+double test2(double *a, int rank, int size)
+{
+	double s=0;
+	int i;
+	if(rank==0)
+	{
+		for(i=0;i<size;i++)
+				a[i] = 505*i;
+
+		for(i=0;i<size;i++)
+				s += a[i];
+	}
+	else
+		s=a[size-1];
+	MPI_Barrier(MPI_COMM_WORLD);
+	return s;
+}
+
+double test3(double *a, int rank, int size)
+{
+	double s=test1(a,rank,size);
+	return s;
 }
--- test/ats/fortran/mpi_patt_all_test.f90
+++ test/ats/fortran/mpi_patt_all_test.f90
@@ -1,3 +1,5 @@
+#include <SCOREP_User.inc>
+
 PROGRAM MPI_PATT_ALL_TEST
 
 USE MPI_PATTERN
@@ -6,25 +8,32 @@ IMPLICIT NONE
 
 INTEGER :: ierror,k
 TYPE(val_distr_t) :: dd
+SCOREP_USER_REGION_DEFINE(mainRegion)
 
 CALL MPI_INIT(ierror)
 CALL set_base_comm(MPI_INTEGER, 20000)
 
 
-do k=1,10
-!$MON USER REGION
+do k=1,1
+
+
+SCOREP_USER_OA_PHASE_BEGIN(mainRegion,"mainRegion",SCOREP_USER_REGION_TYPE_COMMON)
+
+
+
 dd = val_distr_t(0D0, 0.5D0, 1D0, 0D0, 0)
-CALL imbalance_at_mpi_barrier(df_block2, dd, 2, MPI_COMM_WORLD)
-!CALL late_sender(0.1D0, 0.2D0, 14, MPI_COMM_WORLD)
-!CALL late_broadcast(0.2D0, 0.3D0, 1, 13, MPI_COMM_WORLD)
-!CALL late_receiver(0.1D0, 0.2D0, 15, MPI_COMM_WORLD)
-!CALL late_scatter(0.4D0, 0.1D0, 2, 5, MPI_COMM_WORLD)
-!CALL late_scatterv(0.2D0, 0.7D0, 3, 3, MPI_COMM_WORLD)
-!CALL imbalance_at_mpi_alltoall(df_cyclic2, dd, 7, MPI_COMM_WORLD)
-!CALL early_reduce(0.1D0, 0.5D0, 4, 9, MPI_COMM_WORLD)
-!CALL early_gather(0.2D0, 0.3D0, 5, 4, MPI_COMM_WORLD)
-!CALL early_gatherv(0.3D0, 0.2D0, 6, 7, MPI_COMM_WORLD)
-!$MON END USER REGION
+CALL imbalance_at_mpi_barrier(df_block2, dd, 1, MPI_COMM_WORLD)
+CALL late_sender(0D0, 0.5D0, 1, MPI_COMM_WORLD)
+CALL late_broadcast(0D0, 0.5D0, 1, 1, MPI_COMM_WORLD)
+CALL late_scatter(0D0, 0.5D0, 1, 1, MPI_COMM_WORLD)
+CALL imbalance_at_mpi_alltoall(df_cyclic2, dd, 1, MPI_COMM_WORLD)
+CALL early_reduce(0D0, 0.5D0, 1, 1, MPI_COMM_WORLD)
+CALL early_gather(0D0, 0.5D0, 1, 1, MPI_COMM_WORLD)
+
+
+
+SCOREP_USER_OA_PHASE_END(mainRegion)
+
 enddo
 CALL MPI_FINALIZE(ierror)
 
