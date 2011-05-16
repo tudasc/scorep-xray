188932d4b18ab16c8f46b883f90ee2e1243cc3b5
Bert Wesarg <bert.wesarg@tu-dresden.de>
2011-05-16 14:01:38 +0000

    Beautify
    Subversion: revision:1522

--- src/services/scorep_platform_bgp.c
+++ src/services/scorep_platform_bgp.c
@@ -36,58 +36,64 @@ SCOREP_Platform_GetSystemTree( size_t* number_of_entries )
     SCOREP_Platform_SystemTreeNode* path;
     *number_of_entries = 0;
 
-    _BGP_Personality_t mybgp;  
+    _BGP_Personality_t                mybgp;
     _BGP_UniversalComponentIdentifier uci;
-    Kernel_GetPersonality(&mybgp, sizeof(_BGP_Personality_t));
+    Kernel_GetPersonality( &mybgp, sizeof( _BGP_Personality_t ) );
     uci.UCI = mybgp.Kernel_Config.UniversalComponentIdentifier;
 
-    if ((uci.ComputeCard.Component == _BGP_UCI_Component_ComputeCard) ||
-        (uci.IOCard.Component == _BGP_UCI_Component_IOCard)) 
+    if ( ( uci.ComputeCard.Component == _BGP_UCI_Component_ComputeCard ) ||
+         ( uci.IOCard.Component == _BGP_UCI_Component_IOCard ) )
     {
-         unsigned rack_row = uci.ComputeCard.RackRow;
-         unsigned rack_col = uci.ComputeCard.RackColumn;
-         unsigned midplane = uci.ComputeCard.Midplane;
-         unsigned nodecard = uci.ComputeCard.NodeCard;
+        unsigned rack_row = uci.ComputeCard.RackRow;
+        unsigned rack_col = uci.ComputeCard.RackColumn;
+        unsigned midplane = uci.ComputeCard.Midplane;
+        unsigned nodecard = uci.ComputeCard.NodeCard;
 
-         /* Allocate memory for path */
-         path = (SCOREP_Platform_SystemTreeNode*) 
-	   malloc(5 * sizeof(SCOREP_Platform_SystemTreeNode));
-	 if (path == NULL) return NULL;
+        /* Allocate memory for path */
+        path = ( SCOREP_Platform_SystemTreeNode* )
+               malloc( 5 * sizeof( SCOREP_Platform_SystemTreeNode ) );
+        if ( path == NULL )
+        {
+            return NULL;
+        }
 
-         /* Allocate memory for name strings */
-         for (int i=0; i<5; i++)
-	 {
-             path[i].name = (char*) malloc (32*sizeof(char));
-             if (path[i].name == NULL)
-             {
-	          SCOREP_Platform_FreePath(path, i-1);
-                  return 0;
-	     }
-	 }
+        /* Allocate memory for name strings */
+        for ( int i = 0; i < 5; i++ )
+        {
+            path[ i ].name = ( char* )malloc( 32 * sizeof( char ) );
+            if ( path[ i ].name == NULL )
+            {
+                SCOREP_Platform_FreePath( path, i - 1 );
+                return 0;
+            }
+        }
 
-         path[0].class = "machine";
-         path[0].name = "BlueGene/P";
-         path[1].class = "rack row";
-         sprintf(path[1].name, "%u", rack_row);
-         path[2].name = "rack column";
-         sprintf(path[2].name, "%u", rack_col);
-         path[3].name = "midplane";
-         sprintf(path[3].name, "%u", midplane);
-         path[4].name = "nodecard";
-         sprintf(path[4].name, "%u", nodecard);
-         *number_of_entries = 5;
-         return path;
+        path[ 0 ].class = "machine";
+        path[ 0 ].name  = "BlueGene/P";
+        path[ 1 ].class = "rack row";
+        sprintf( path[ 1 ].name, "%u", rack_row );
+        path[ 2 ].name = "rack column";
+        sprintf( path[ 2 ].name, "%u", rack_col );
+        path[ 3 ].name = "midplane";
+        sprintf( path[ 3 ].name, "%u", midplane );
+        path[ 4 ].name = "nodecard";
+        sprintf( path[ 4 ].name, "%u", nodecard );
+        *number_of_entries = 5;
+        return path;
     }
     else
     {
-         /* Allocate memory for path */
-         path = (SCOREP_Platform_SystemTreeNode*) 
-	   malloc(sizeof(SCOREP_Platform_SystemTreeNode));
-	 if (path == NULL) return NULL;
-         path[0].class = "machine";
-         path[0].name = "BlueGene/P";
-         *number_of_entries = 1;
-         return path;
+        /* Allocate memory for path */
+        path = ( SCOREP_Platform_SystemTreeNode* )
+               malloc( sizeof( SCOREP_Platform_SystemTreeNode ) );
+        if ( path == NULL )
+        {
+            return NULL;
+        }
+        path[ 0 ].class    = "machine";
+        path[ 0 ].name     = "BlueGene/P";
+        *number_of_entries = 1;
+        return path;
     }
 }
 
@@ -95,9 +101,9 @@ void
 SCOREP_Platform_FreePath( SCOREP_Platform_SystemTreeNode* path,
                           size_t                          number_of_entries )
 {
-    for (int i=1; i<number_of_entries; i++)
+    for ( int i = 1; i < number_of_entries; i++ )
     {
-        free(path[i].name);   
+        free( path[ i ].name );
     }
-    free(path);
+    free( path );
 }
