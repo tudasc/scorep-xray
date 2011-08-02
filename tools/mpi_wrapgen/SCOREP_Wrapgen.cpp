3ff0558361612629707f8068d23afa8e171e730b
Daniel Lorenz <lorenz@cs.tu-darmstadt.de>
2011-08-02 11:36:58 +0000

    Included Sclasca copyright notice before ours
    Subversion: revision:1723

--- tools/mpi_wrapgen/SCOREP_Wrapgen.cpp
+++ tools/mpi_wrapgen/SCOREP_Wrapgen.cpp
@@ -14,6 +14,22 @@
  *
  */
 
+/****************************************************************************
+**  SCALASCA    http://www.scalasca.org/                                   **
+*****************************************************************************
+**  Copyright (c) 1998-2011                                                **
+**  Forschungszentrum Juelich GmbH, Juelich Supercomputing Centre          **
+**                                                                         **
+**  Copyright (c) 2010-2011                                                **
+**  German Research School for Simulation Sciences GmbH,                   **
+**  Laboratory for Parallel Programming                                    **
+**                                                                         **
+**  Copyright (c) 2003-2008                                                **
+**  University of Tennessee, Innovative Computing Laboratory               **
+**                                                                         **
+**  See the file COPYRIGHT in the package base directory for details       **
+****************************************************************************/
+
 /**
  * @file       SCOREP_Wrapgen.cpp
  * @maintainer Daniel Lorenz <d.lorenz@fz-juelich.de>
@@ -22,6 +38,7 @@
  *
  * @brief Wrapper generator for automatic creation of MPI wrappers
  */
+
 #include <config.h>
 
 #include <cstdio>
--- tools/mpi_wrapgen/SCOREP_Wrapgen_Func.h
+++ tools/mpi_wrapgen/SCOREP_Wrapgen_Func.h
@@ -14,6 +14,22 @@
  *
  */
 
+/****************************************************************************
+**  SCALASCA    http://www.scalasca.org/                                   **
+*****************************************************************************
+**  Copyright (c) 1998-2011                                                **
+**  Forschungszentrum Juelich GmbH, Juelich Supercomputing Centre          **
+**                                                                         **
+**  Copyright (c) 2010-2011                                                **
+**  German Research School for Simulation Sciences GmbH,                   **
+**  Laboratory for Parallel Programming                                    **
+**                                                                         **
+**  Copyright (c) 2003-2008                                                **
+**  University of Tennessee, Innovative Computing Laboratory               **
+**                                                                         **
+**  See the file COPYRIGHT in the package base directory for details       **
+****************************************************************************/
+
 #ifndef SCOREP_FUNC_H
 #define SCOREP_FUNC_H
 
@@ -65,7 +81,8 @@ public:
           const std::string & guard,
           const paramlist_t & params );
 
-    virtual ~
+    virtual
+    ~
     Func();
 
     /** return unique identifier of function
