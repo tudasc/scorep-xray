569d98161819a4a33f5be26c48cef81ec848ec03
Daniel Lorenz <lorenz@cs.tu-darmstadt.de>
2011-08-18 14:48:37 +0000

    Added test for c++ user adapter, instrumentation of constructor and destructor and using omp locks in constructor and destructor.
    Subversion: revision:1818

---
+++ test/adapters/user/C++/run_user_cxx_test.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+## 
+## This file is part of the Score-P software (http://www.score-p.org)
+##
+## Copyright (c) 2009-2011, 
+##    RWTH Aachen University, Germany
+##    Gesellschaft fuer numerische Simulation mbH Braunschweig, Germany
+##    Technische Universitaet Dresden, Germany
+##    University of Oregon, Eugene, USA
+##    Forschungszentrum Juelich GmbH, Germany
+##    German Research School for Simulation Sciences GmbH, Juelich/Aachen, Germany
+##    Technische Universitaet Muenchen, Germany
+##
+## See the COPYING file in the package base directory for details.
+##
+
+## file       run_filter_test.sh
+## maintainer Daniel Lorenz <d.lorenz@fz-juelich.de>
+
+# Get SRC_ROOT and OTF2_PRINT
+make clean-local-scorep-config-tool
+make scorep-config-tool-local
+. ./scorep_config.dat
+
+# Execute test without OpenMP and compiler instrumentation (only user)
+echo "/* Dummy */" > config.h
+./scorep --verbosity=1 --config=scorep_config.dat --user --nocompiler $CXX -I. -o user_cxx_test $SRC_ROOT/test/adapters/user/C++/user_test.cxx
+if [ ! -e user_cxx_test ]; then
+    rm -f config.h
+    exit 1
+fi
+
+./user_cxx_test
+if [ $? -ne 0 ]; then
+    rm -rf scorep-measurement-tmp config.h user_cxx_test
+    exit 1
+fi
+rm -f config.h user_cxx_test
+
+exit 0
+
+
+# Execute test with OpenMP compiler and user instrumentation.
+echo "/* Dummy */" > config.h
+./scorep --verbosity=1 --config=scorep_config.dat --user $CXX $OPENMP_CFLAGS -I. -o user_cxx_test $SRC_ROOT/test/adapters/user/C++/user_test.cxx
+if [ ! -e user_cxx_test ]; then
+    rm -f config.h
+    exit 1
+fi
+
+./user_cxx_test
+if [ $? -ne 0 ]; then
+    rm -rf scorep-measurement-tmp config.h user_cxx_test
+    exit 1
+fi
+rm -f config.h user_cxx_test
+
+exit 0
+
---
+++ test/adapters/user/C++/user_test.cxx
@@ -0,0 +1,67 @@
+/*
+ * This file is part of the Score-P software (http://www.score-p.org)
+ *
+ * Copyright (c) 2009-2011,
+ *    RWTH Aachen University, Germany
+ *    Gesellschaft fuer numerische Simulation mbH Braunschweig, Germany
+ *    Technische Universitaet Dresden, Germany
+ *    University of Oregon, Eugene, USA
+ *    Forschungszentrum Juelich GmbH, Germany
+ *    German Research School for Simulation Sciences GmbH, Juelich/Aachen, Germany
+ *    Technische Universitaet Muenchen, Germany
+ *
+ * See the COPYING file in the package base directory for details.
+ *
+ */
+
+/**
+ * @ file      user_test.cxx
+ * @maintainer Daniel Lorenz <d.lorenz@fz-juelich.de>
+ *
+ * @brief Tests C++ user instrumentation and instrumentation of static constructors
+ *        and destructors.
+ */
+
+#include <config.h>
+#include <stdio.h>
+#include <SCOREP_User.h>
+
+#ifdef _OPENMP
+#include <omp.h>
+#endif
+
+class MyClass
+{
+public:
+    MyClass()
+    {
+        SCOREP_USER_REGION("MyClass Constructor", 0);
+        printf("In constructor of static class instance\n");
+#ifdef _OPENMP
+        omp_init_lock( &m_lock );
+#endif
+    }
+
+    virtual~ MyClass()
+    {
+        SCOREP_USER_REGION("MyClass Destructor", 0);
+        printf("In destructor of static class instance\n");
+#ifdef _OPENMP
+        omp_destroy_lock( &m_lock );
+#endif
+    }
+
+private:
+#ifdef _OPENMP
+    omp_lock_t m_lock;
+#endif
+};
+
+
+MyClass my_static_instance;
+
+int main()
+{
+    SCOREP_USER_REGION("main", 0);
+    printf("In main\n");
+}
