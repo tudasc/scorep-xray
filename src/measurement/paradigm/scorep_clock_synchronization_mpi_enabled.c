b74adf1376e4343df8e42958a331595728ed7334
Christian Rössel <c.roessel@fz-juelich.de>
2011-11-04 19:11:05 +0000

    This is weird! In SCOREP_SynchronizeClocks() the loop index i of
    {{{
     for ( int i = 0; i < N_PINGPONGS; ++i )
    }}}
    
    got reproducibly corrupted. Renaming didn't help either. Had to extract it to
    a function. Additionally we need a tag for the ping pongs. The
    SCOREP_Mpi_Send/Recv don't provide this yet.
    
    
    Subversion: revision:2099

--- src/measurement/paradigm/scorep_clock_synchronization_mpi_enabled.c
+++ src/measurement/paradigm/scorep_clock_synchronization_mpi_enabled.c
@@ -30,11 +30,19 @@
 #include <SCOREP_Timing.h>
 #include <scorep_mpi.h>
 
+#include <mpi.h>
+#include <stddef.h>
+
 #define N_PINGPONGS 10
+
+uint64_t master_send_time[ N_PINGPONGS ];
+uint64_t master_recv_time[ N_PINGPONGS ];
 
 /* *INDENT-OFF* */
 extern void scorep_interpolate_epoch(uint64_t* epochBegin, uint64_t* epochEnd);
+static void scorep_synchronize_master( int worker, uint64_t* ping_pong_time, int* min_index );
 /* *INDENT-ON*  */
+
 
 void
 SCOREP_SynchronizeClocks()
@@ -50,30 +58,14 @@ SCOREP_SynchronizeClocks()
 
     if ( SCOREP_Mpi_GetRank() == 0 )
     {
+        SCOREP_Mpi_Status status;
         for ( int worker = 1; worker < SCOREP_Mpi_GetCommWorldSize(); ++worker )
         {
-            uint64_t          master_send_time[ N_PINGPONGS ];
-            uint64_t          master_recv_time[ N_PINGPONGS ];
-            SCOREP_Mpi_Status status;
-            for ( int i = 0; i < N_PINGPONGS; ++i )
-            {
-                master_send_time[ i ] = SCOREP_GetClockTicks();
-                SCOREP_Mpi_Send( NULL, 0, SCOREP_MPI_INT, worker );
-                SCOREP_Mpi_Recv( NULL, 0, SCOREP_MPI_INT, worker, &status );
-                master_recv_time[ i ] = SCOREP_GetClockTicks();
-            }
-
-            uint64_t ping_pong_time = master_recv_time[ 0 ] - master_send_time[ 0 ];
-            int      min_index      = 0;
-            for ( int i = 1; i < N_PINGPONGS; ++i )
-            {
-                if ( master_recv_time[ i ] - master_send_time[ i ] < ping_pong_time )
-                {
-                    ping_pong_time = master_recv_time[ i ] - master_send_time[ i ];
-                    min_index      = i;
-                }
-            }
+            uint64_t ping_pong_time;
+            int      min_index = 0;
+            scorep_synchronize_master( worker, &ping_pong_time, &min_index );
             uint64_t sync_time = master_send_time[ min_index ] + ping_pong_time / 2;
+
             SCOREP_Mpi_Send( &sync_time, 1, SCOREP_MPI_UNSIGNED_LONG_LONG, worker );
             SCOREP_Mpi_Send( &min_index, 1, SCOREP_MPI_INT, worker );
         }
@@ -83,15 +75,18 @@ SCOREP_SynchronizeClocks()
     }
     else
     {
-        uint64_t          worker_time[ N_PINGPONGS ];
-        SCOREP_Mpi_Status status;
-        int               master = 0;
+        uint64_t   worker_time[ N_PINGPONGS ];
+        MPI_Status status;
+        int        master = 0;
         for ( int i = 0; i < N_PINGPONGS; ++i )
         {
-            SCOREP_Mpi_Recv( NULL, 0, SCOREP_MPI_INT, master, &status );
+            //SCOREP_Mpi_Recv( NULL, 0, SCOREP_MPI_INT, master, &status );
+            PMPI_Recv( NULL, 0, MPI_INT, master, 1, MPI_COMM_WORLD, &status );
             worker_time[ i ] = SCOREP_GetClockTicks();
-            SCOREP_Mpi_Send( NULL, 0, SCOREP_MPI_INT, master );
+            //SCOREP_Mpi_Send( NULL, 0, SCOREP_MPI_INT, master );
+            PMPI_Send( NULL, 0, MPI_INT, master, 2, MPI_COMM_WORLD );
         }
+
         uint64_t sync_time;
         int      min_index;
         SCOREP_Mpi_Recv( &sync_time, 1, SCOREP_MPI_UNSIGNED_LONG_LONG, master, &status );
@@ -103,6 +98,33 @@ SCOREP_SynchronizeClocks()
 
     uint64_t stddev_not_calculated_yet = 0;
     SCOREP_AddClockOffset( offset_time, offset, stddev_not_calculated_yet );
+}
+
+
+static void
+scorep_synchronize_master( int worker, uint64_t* ping_pong_time, int* min_index )
+{
+    MPI_Status status;
+    int        foox;
+    for ( int i = 0; i < N_PINGPONGS; ++i )
+    {
+        master_send_time[ i ] = SCOREP_GetClockTicks();
+        //SCOREP_Mpi_Send( NULL, 0, SCOREP_MPI_INT, worker );
+        //SCOREP_Mpi_Recv( NULL, 0, SCOREP_MPI_INT, worker, &status );
+        PMPI_Send( NULL, 0, MPI_INT, worker, 1, MPI_COMM_WORLD );
+        PMPI_Recv( NULL, 0, MPI_INT, worker, 2, MPI_COMM_WORLD, &status );
+        master_recv_time[ i ] = SCOREP_GetClockTicks();
+    }
+
+    *ping_pong_time = master_recv_time[ 0 ] - master_send_time[ 0 ];
+    for ( int i = 1; i < N_PINGPONGS; ++i )
+    {
+        if ( master_recv_time[ i ] - master_send_time[ i ] < *ping_pong_time )
+        {
+            *ping_pong_time = master_recv_time[ i ] - master_send_time[ i ];
+            *min_index      = i;
+        }
+    }
 }
 
 
