32cbb6e5776bfbc6b5aac5f7a0b4927fb3dcdd66
Bert Wesarg <bert.wesarg@tu-dresden.de>
2011-12-12 20:00:10 +0000

    Write space optimaized mappings into the local definitions.
    Subversion: revision:2272

--- src/measurement/scorep_definition_macros.h
+++ src/measurement/scorep_definition_macros.h
@@ -540,24 +540,20 @@
              scorep_local_definition_manager.mappings->type ## _mappings && \
              scorep_local_definition_manager.type ## _definition_counter > 0 ) \
         { \
-            OTF2_IdMap* map = OTF2_IdMap_Create( \
-                OTF2_ID_MAP_DENSE, \
-                scorep_local_definition_manager.type ## _definition_counter ); \
-            for ( uint32_t index = 0; \
-                  index < scorep_local_definition_manager.type ## _definition_counter; \
-                  ++index ) \
+            OTF2_IdMap* map = OTF2_IdMap_CreateFromUint32Array( \
+                scorep_local_definition_manager.type ## _definition_counter, \
+                scorep_local_definition_manager.mappings->type ## _mappings, \
+                true ); \
+            /* map can be NULL if the mapping is the identity */ \
+            if ( map ) \
             { \
-                OTF2_IdMap_AddIdPair( \
+                SCOREP_Error_Code status = OTF2_DefWriter_WriteMappingTable( \
+                    definition_writer, \
                     map, \
-                    index, \
-                    scorep_local_definition_manager.mappings->type ## _mappings[ index ] ); \
+                    OTF2_MAPPING_ ## TYPE ); \
+                assert( status == SCOREP_SUCCESS ); \
+                OTF2_IdMap_Free( map ); \
             } \
-            SCOREP_Error_Code status = OTF2_DefWriter_WriteMappingTable( \
-                definition_writer, \
-                map, \
-                OTF2_MAPPING_ ## TYPE ); \
-            assert( status == SCOREP_SUCCESS ); \
-            OTF2_IdMap_Free( map ); \
         } \
     } \
     while ( 0 )
