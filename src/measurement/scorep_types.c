030841b8c17d906a6dbadf6938fd7661fd00c474
Daniel Lorenz <lorenz@cs.tu-darmstadt.de>
2012-03-02 14:03:36 +0000

    Just for triggering new test build after last OTF2 changes
    Subversion: revision:2617

--- src/measurement/scorep_types.c
+++ src/measurement/scorep_types.c
@@ -305,8 +305,8 @@ scorep_region_type_to_string
             return "omp single sblock";
         case SCOREP_REGION_OMP_TASK:
             return "omp task";
-        case SCOREP_REGION_OMP_CREATE_TASK:
-            return "omp create task";
+        case SCOREP_REGION_OMP_TASK_CREATE:
+            return "omp task create";
         default:
             return "unknown";
     }
