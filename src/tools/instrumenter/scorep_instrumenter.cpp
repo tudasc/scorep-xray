45c186d8323132572c2e22d81d8926a52d641b82
Bert Wesarg <bert.wesarg@tu-dresden.de>
2012-09-04 08:28:24 +0000

    Fix missing " " after scorep-config call.
    Subversion: revision:3312

--- src/tools/instrumenter/scorep_instrumenter.cpp
+++ src/tools/instrumenter/scorep_instrumenter.cpp
@@ -315,7 +315,7 @@ SCOREP_Instrumenter::prepare_config_tool_calls( std::string input_file )
     // Generate calls
     m_compiler_flags = "`" + scorep_config + mode + " --cflags` ";
     m_linker_flags   = "`" + scorep_config + mode + " --ldflags` `" +
-                       scorep_config + mode + " --libs`";
+                       scorep_config + mode + " --libs` ";
     if ( m_command_line->isOpariInstrumenting() )
     {
         m_compiler_flags += "`" + m_install_data->getOpariConfig()
