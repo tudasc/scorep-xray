34203f0b4c5faa4ad142c90a9014fde3fa1e490c
Bert Wesarg <bert.wesarg@tu-dresden.de>
2012-09-18 06:27:12 +0000

    Fix HAVE() macro.
    Subversion: common:revision:885

--- config-common.h
+++ config-common.h
@@ -37,8 +37,8 @@
  * 1, you should use <tt>#if HAVE(DECL_MPI_ACCUMULATE)</tt> in your code
  * to conditionally compile if @c HAVE_DECL_MPI_ACCUMULATE is defined to 1.
  */
-#define HAVE_( H ) ( defined( HAVE_ ## H ) && HAVE_ ## H )
-#define HAVE( H )  HAVE_( H )
+#define HAVE2( H ) ( defined( HAVE_ ## H ) && HAVE_ ## H )
+#define HAVE( H )  HAVE2( H )
 
 
 /**
