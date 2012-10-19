3d2600872b1022928b674d2a221cd583c6684449
Dirk Schmidl <schmidl@rz.rwth-aachen.de>
2012-10-19 14:17:14 +0000

    - added source for nested OpenMP jacobi examples - after compilation they can be started with "OMP_NUM_THREADS=2,4 jacobi.exe" if OpenMP >3.1 is supported, otherwise with "OMP_NUM_THREADS=8 jacobi.exe" the total threads are then distributed over two parallel regions
    Subversion: revision:3554

---
+++ test/installcheck/omp/src/nested_jacobi/cxx/jacobi.h
@@ -0,0 +1,60 @@
+#ifndef _JACOBI_H
+#define _JACOBI_H
+
+struct JacobiData
+{
+    // input data
+    int    iRows;
+    int    iCols;
+    int    iRowFirst;
+    int    iRowLast;
+    int    iIterMax;
+    double fAlpha;
+    double fRelax;
+    double fTolerance;
+
+    // calculated dx & dy
+    double fDx;
+    double fDy;
+
+    // pointers to the allocated memory
+    double* afU;
+    double* afF;
+
+    // start and end timestamps
+    double fTimeStart;
+    double fTimeStop;
+
+    // calculated residual (output jacobi)
+    double fResidual;
+    // effective interation count (output jacobi)
+    int    iIterCount;
+
+    // calculated error (output error_check)
+    double fError;
+
+    // MPI-Variables
+    int iMyRank;       // current process rank (number)
+    int iNumProcs;     // how many processes
+
+    // threadsnumbers in nested parallel regions
+    int outer_threads;
+    int inner_threads;
+};
+
+// jacobi calculation routine
+void
+Jacobi( JacobiData &data );
+
+// final cleanup routines
+void
+Finish( JacobiData &data );
+
+// routine to update the residual
+// needed to have a function call
+// in the parallel region for tests
+void
+update_residual( double &residual,
+                 double  fLRes );
+
+#endif
---
+++ test/installcheck/omp/src/nested_jacobi/fc/VariableDef.f90
@@ -0,0 +1,52 @@
+module VariableDef
+    implicit none
+
+    TYPE JacobiData
+        integer :: iRows
+        integer :: iCols
+        integer :: iRowFirst
+        integer :: iRowLast
+        integer :: iIterMax
+        double precision :: fAlpha
+        double precision :: fRelax
+        double precision :: fTolerance
+    
+!        /* calculated dx & dy */
+        double precision :: fDx
+        double precision :: fDy
+
+!       /* pointers to the allocated memory */
+        double precision, allocatable :: afU(:,:)
+        double precision, allocatable :: afF(:,:)
+    
+!       /* start and end timestamps */
+        double precision :: fTimeStart
+        double precision :: fTimeStop
+
+!       /* calculated residual (output jacobi) */
+        double precision :: fResidual
+!       /* effective interation count (output jacobi) */
+        integer :: iIterCount
+
+!       /* calculated error (output error_check) */
+        double precision :: fError
+    
+!       /* MPI-Variables */
+        integer :: iMyRank   !/* current process rank (number) */
+        integer :: iNumProcs !/* how many processes */
+!       /* Number of threads for nested OpenMP*/
+        integer :: outer_threads
+        integer :: inner_threads
+
+   END TYPE JacobiData
+
+   contains
+
+   subroutine update_residual(residual, fLRes)
+       implicit none
+       double precision, intent(inout) :: residual
+       double precision, intent(in) :: fLRes
+       residual = residual + fLRes * fLRes
+   end subroutine
+
+end module VariableDef
