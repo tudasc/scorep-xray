b6e1211de249a4a4feb8a81838c680dd7204725c
Ronny Tschüter <ronny.tschueter@tu-dresden.de>
2012-10-22 08:10:42 +0000

    Update copyright
    Subversion: revision:3561

--- src/services/include/SCOREP_Platform.h
+++ src/services/include/SCOREP_Platform.h
@@ -4,7 +4,7 @@
 /*
  * This file is part of the Score-P software (http://www.score-p.org)
  *
- * Copyright (c) 2009-2011,
+ * Copyright (c) 2009-2012,
  *    RWTH Aachen University, Germany
  *    Gesellschaft fuer numerische Simulation mbH Braunschweig, Germany
  *    Technische Universitaet Dresden, Germany
