a7e7055a4dce30a7cb8a635414a7177d8fd965f0
Christian Rössel <c.roessel@fz-juelich.de>
2013-05-29 09:25:33 +0000

    Provide an extra `-I.` when OPARI2 potentially adds an `#include` statement into Fortran files. With this `#include` the Fortran CRAY compiler chokes on subsequent Fortran `'include'` statements. See ticket:687.
    Subversion: revision:4476

--- src/tools/instrumenter/scorep_instrumenter.cpp
+++ src/tools/instrumenter/scorep_instrumenter.cpp
@@ -211,6 +211,12 @@ SCOREP_Instrumenter::Run( void )
                         {
                             current_file = preprocess_opari( current_file );
                         }
+                        #if HAVE( BACKEND_COMPILER_CRAY )
+                        else if ( m_command_line.getCompilerName().find( "ftn" ) != std::string::npos )
+                        {
+                            m_compiler_flags += " -I.";
+                        }
+                        #endif
                         current_file = instrument_opari( current_file );
                     }
 
