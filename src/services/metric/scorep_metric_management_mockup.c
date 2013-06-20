fa07b5035fda66467f8ecee3dc9945c84303da41
Ronny Tschüter <ronny.tschueter@tu-dresden.de>
2013-06-20 11:23:35 +0000

    Rename scorep_metric_management_without_sources.c to scorep_metric_management_mockup.c.
    Subversion: revision:4602

--- src/services/metric/scorep_metric_management_mockup.c
+++ src/services/metric/scorep_metric_management_mockup.c
@@ -17,7 +17,7 @@
 /**
  *  @status     alpha
  *
- *  @file       scorep_metric_management_without_sources.c
+ *  @file       scorep_metric_management_mockup.c
  *
  *  @author     Ronny Tschueter <ronny.tschueter@zih.tu-dresden.de>
  *  @maintainer Ronny Tschueter <ronny.tschueter@zih.tu-dresden.de>
@@ -78,84 +78,23 @@ SCOREP_Metric_WriteToProfile( SCOREP_Location* location )
 {
 }
 
-/** @brief Called on deregistration of the metric service.
- */
-static void
-scorep_metric_deregister( void )
-{
-}
-
-/** @brief Location specific finalization function for metric services.
- *
- *  @param location             Location data.
- *
- *  @param location Reference to location that will finalize its metric related data structures.
- */
-static void
-scorep_metric_finalize_location( SCOREP_Location* location )
-{
-}
-
-/** @brief Service finalization.
- */
-static void
-scorep_metric_finalize_service( void )
-{
-}
-
-/** @brief  Location specific initialization function for metric services.
- *
- *  @param location             Location data.
- *
- *  @return It returns SCOREP_SUCCESS if successful,
- *          otherwise an error code will be reported.
- */
-static SCOREP_ErrorCode
-scorep_metric_initialize_location( SCOREP_Location* location )
-{
-    return SCOREP_SUCCESS;
-}
-
-/** @brief Called on initialization of the metric service.
- *
- *  @return It returns SCOREP_SUCCESS if successful,
- *          otherwise an error code will be reported.
- */
-static SCOREP_ErrorCode
-scorep_metric_initialize_service( void )
-{
-    return SCOREP_SUCCESS;
-}
-
-/** @brief Registers configuration variables for the metric services.
- *
- *  @param subsystem_id         Identifier of metric subsystem.
- *
- *  @return It returns SCOREP_SUCCESS if successful,
- *          otherwise an error code will be reported.
- */
-static SCOREP_ErrorCode
-scorep_metric_register( size_t subsystem_id )
-{
-    return SCOREP_SUCCESS;
-}
 
 /* *********************************************************************
  * Subsystem declaration
  **********************************************************************/
 
 /**
- * Implementation of the metric service initialization/finalization struct
+ * Dummy implementation of the metric service
  */
 const SCOREP_Subsystem SCOREP_Subsystem_MetricService =
 {
     .subsystem_name              = "METRIC",
-    .subsystem_register          = &scorep_metric_register,
-    .subsystem_init              = &scorep_metric_initialize_service,
-    .subsystem_init_location     = &scorep_metric_initialize_location,
-    .subsystem_finalize_location = &scorep_metric_finalize_location,
+    .subsystem_register          = NULL,
+    .subsystem_init              = NULL,
+    .subsystem_init_location     = NULL,
+    .subsystem_finalize_location = NULL,
     .subsystem_pre_unify         = NULL,
     .subsystem_post_unify        = NULL,
-    .subsystem_finalize          = &scorep_metric_finalize_service,
-    .subsystem_deregister        = &scorep_metric_deregister
+    .subsystem_finalize          = NULL,
+    .subsystem_deregister        = NULL
 };
