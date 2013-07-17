0f2005fefe12a67e0fdea02240c0b80fd2eaf44f
Ronny Tschüter <ronny.tschueter@tu-dresden.de>
2013-07-17 12:51:30 +0000

    Checks to proof whether scorep/SCOREP_MetricPlugins.h and scorep/SCOREP_MetricTypes.h are self-contained.
    Subversion: revision:4719

---
+++ test/public_headers/self_contained_public_header_SCOREP_MetricTypes.c
@@ -0,0 +1,38 @@
+/*
+ * This file is part of the Score-P software (http://www.score-p.org)
+ *
+ * Copyright (c) 2009-2013,
+ *    RWTH Aachen University, Germany
+ *    Gesellschaft fuer numerische Simulation mbH Braunschweig, Germany
+ *    Technische Universitaet Dresden, Germany
+ *    University of Oregon, Eugene, USA
+ *    Forschungszentrum Juelich GmbH, Germany
+ *    German Research School for Simulation Sciences GmbH, Juelich/Aachen, Germany
+ *    Technische Universitaet Muenchen, Germany
+ *
+ * See the COPYING file in the package base directory for details.
+ *
+ */
+
+
+/**
+ * @file       self_contained_public_header_SCOREP_MetricTypes.c
+ * @author     Ronny Tschueter <ronny.tschueter@tu-dresden.de>
+ * @maintainer Ronny Tschueter <ronny.tschueter@tu-dresden.de>
+ *
+ * @status alpha
+ *
+ *
+ */
+
+
+#include <config.h>
+
+#include <scorep/SCOREP_MetricTypes.h>
+
+
+int
+main()
+{
+    return 0;
+}
