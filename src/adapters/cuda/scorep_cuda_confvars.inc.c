8d4e745dbad7fa929918c2780058f77d3ddb1170
Ronny Tschüter <ronny.tschueter@tu-dresden.de>
2013-07-22 07:54:58 +0000

    Update options of SCOREP_ENABLE_CUDA in user documentation
    Subversion: revision:4741

--- src/adapters/cuda/scorep_cuda_confvars.inc.c
+++ src/adapters/cuda/scorep_cuda_confvars.inc.c
@@ -34,24 +34,23 @@ size_t   scorep_cupti_activity_buffer_size;
  * (bit mask).
  */
 static const SCOREP_ConfigType_SetEntry scorep_cuda_enable_groups[] = {
-    { "runtime",        SCOREP_CUDA_FEATURE_RUNTIME_API                                    },
-    { "driver",         SCOREP_CUDA_FEATURE_DRIVER_API                                     },
-    { "kernel",         SCOREP_CUDA_FEATURE_KERNEL                                         },
-    { "kernel_counter", SCOREP_CUDA_FEATURE_KERNEL_COUNTER                                 },
-    { "concurrent",     SCOREP_CUDA_FEATURE_CONCURRENT_KERNEL                              },
-    { "memcpy",         SCOREP_CUDA_FEATURE_MEMCPY                                         },
-    { "sync",           SCOREP_CUDA_FEATURE_SYNC                                           },
-    { "gpu",            SCOREP_CUDA_FEATURE_KERNEL | SCOREP_CUDA_FEATURE_MEMCPY            },
-    { "idle",           SCOREP_CUDA_FEATURE_IDLE                                           },
-    { "pure_idle",      SCOREP_CUDA_FEATURE_PURE_IDLE                                      },
-    { "device_reuse",   SCOREP_CUDA_FEATURE_DEVICE_REUSE                                   },
-    { "stream_reuse",   SCOREP_CUDA_FEATURE_STREAM_REUSE                                   },
-    { "gpumemusage",    SCOREP_CUDA_FEATURE_GPUMEMUSAGE                                    },
-    { "DEFAULT",        SCOREP_CUDA_FEATURES_DEFAULT                                       },
-    { "1",              SCOREP_CUDA_FEATURES_DEFAULT                                       },
-    { "yes",            SCOREP_CUDA_FEATURES_DEFAULT                                       },
-    { "no",             0                                                                  },
-    { NULL,             0                                                                  }
+    { "runtime",        SCOREP_CUDA_FEATURE_RUNTIME_API           },
+    { "driver",         SCOREP_CUDA_FEATURE_DRIVER_API            },
+    { "kernel",         SCOREP_CUDA_FEATURE_KERNEL                },
+    { "kernel_counter", SCOREP_CUDA_FEATURE_KERNEL_COUNTER        },
+    { "concurrent",     SCOREP_CUDA_FEATURE_CONCURRENT_KERNEL     },
+    { "memcpy",         SCOREP_CUDA_FEATURE_MEMCPY                },
+    { "sync",           SCOREP_CUDA_FEATURE_SYNC                  },
+    { "idle",           SCOREP_CUDA_FEATURE_IDLE                  },
+    { "pure_idle",      SCOREP_CUDA_FEATURE_PURE_IDLE             },
+    { "device_reuse",   SCOREP_CUDA_FEATURE_DEVICE_REUSE          },
+    { "stream_reuse",   SCOREP_CUDA_FEATURE_STREAM_REUSE          },
+    { "gpumemusage",    SCOREP_CUDA_FEATURE_GPUMEMUSAGE           },
+    { "DEFAULT",        SCOREP_CUDA_FEATURES_DEFAULT              },
+    { "1",              SCOREP_CUDA_FEATURES_DEFAULT              },
+    { "yes",            SCOREP_CUDA_FEATURES_DEFAULT              },
+    { "no",             0                                         },
+    { NULL,             0                                         }
 };
 
 
@@ -67,19 +66,20 @@ static SCOREP_ConfigVariable scorep_cuda_configs[] = {
         "no",                      /* default value */
         "CUDA measurement features",
         "Sets the CUDA measurement mode to capture:\n"
-        "  runtime:       CUDA runtime API\n"
-        "  driver:        CUDA driver API\n"
-        "  gpu:           GPU activities\n"
-        "  kernel:        CUDA kernels\n"
-        "  idle:          GPU compute idle time\n"
-        "  pure_idle:     GPU idle time (memory copies are not idle)\n"
-        "  memcpy:        CUDA memory copies\n"
-        "  sync:          Record implicit and explicit CUDA synchronization\n"
-        "  gpumemusage:   Record CUDA memory (de)allocations as a counter\n"
-        "  stream_reuse:  Reuse destroyed/closed CUDA streams\n"
-        "  device_reuse:  Reuse destroyed/closed CUDA devices\n"
-        "  default/yes/1: CUDA runtime API and GPU activities\n"
-        "  no:            Disable CUDA measurement\n"
+        "  runtime:        CUDA runtime API\n"
+        "  driver:         CUDA driver API\n"
+        "  kernel:         CUDA kernels\n"
+        "  kernel_counter: Fixed CUDA kernel metrics.\n"
+        "  concurrent:     Concurrent kernel recording.\n"
+        "  idle:           GPU compute idle time\n"
+        "  pure_idle:      GPU idle time (memory copies are not idle)\n"
+        "  memcpy:         CUDA memory copies\n"
+        "  sync:           Record implicit and explicit CUDA synchronization\n"
+        "  gpumemusage:    Record CUDA memory (de)allocations as a counter\n"
+        "  stream_reuse:   Reuse destroyed/closed CUDA streams\n"
+        "  device_reuse:   Reuse destroyed/closed CUDA devices\n"
+        "  default/yes/1:  CUDA runtime API and GPU activities\n"
+        "  no:             Disable CUDA measurement\n"
     },
     {
         "buffer",
