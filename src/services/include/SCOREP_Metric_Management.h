846947d9933908f21f34174bc4d0c89a4d75284d
Ronny Tschüter <ronny.tschueter@tu-dresden.de>
2013-08-26 13:12:51 +0000

    Update copyright header of metric service.
    Subversion: revision:4904

--- src/services/include/SCOREP_Metric_Management.h
+++ src/services/include/SCOREP_Metric_Management.h
@@ -3,14 +3,28 @@
  *
  * Copyright (c) 2009-2013,
  *    RWTH Aachen University, Germany
+ *
+ * Copyright (c) 2009-2013,
  *    Gesellschaft fuer numerische Simulation mbH Braunschweig, Germany
+ *
+ * Copyright (c) 2009-2013,
  *    Technische Universitaet Dresden, Germany
+ *
+ * Copyright (c) 2009-2013,
  *    University of Oregon, Eugene, USA
+ *
+ * Copyright (c) 2009-2013,
  *    Forschungszentrum Juelich GmbH, Germany
+ *
+ * Copyright (c) 2009-2013,
  *    German Research School for Simulation Sciences GmbH, Juelich/Aachen, Germany
+ *
+ * Copyright (c) 2009-2013,
  *    Technische Universitaet Muenchen, Germany
  *
- * See the COPYING file in the package base directory for details.
+ * This software may be modified and distributed under the terms of
+ * a BSD-style license.  See the COPYING file in the package base
+ * directory for details.
  *
  */
 
