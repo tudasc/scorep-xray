5549f977e19363921c92e399074c2d7a5c4a11ce
Daniel Lorenz <lorenz@cs.tu-darmstadt.de>
2013-09-19 09:38:29 +0000

    Cray compiler adapter registers exit handler to check whether the first entered region has been exited. Fixes ticket:785
    Subversion: revision:5070

--- src/adapters/compiler/scorep_compiler_cray.c
+++ src/adapters/compiler/scorep_compiler_cray.c
@@ -37,6 +37,13 @@
  */
 
 #include <config.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <SCOREP_RuntimeManagement.h>
+
+static void* scorep_first_func        = NULL;
+static void* scorep_first_callsite    = NULL;
+static bool  scorep_exited_first_func = false;
 
 void
 __cyg_profile_func_enter( void* func,
@@ -45,6 +52,20 @@ __cyg_profile_func_enter( void* func,
 void
 __cyg_profile_func_exit( void* func,
                          void* callsite );
+
+
+/* Checks whether the main function was exited. The Cray Fortran compiler instrumentation
+ * does not issue the exit */
+static int
+scorep_compiler_cray_final_exit( void )
+{
+    if ( !scorep_exited_first_func )
+    {
+        scorep_exited_first_func = true;
+        __cyg_profile_func_exit( scorep_first_func, scorep_first_callsite );
+    }
+    return 0;
+}
 
 /* ***************************************************************************************
    Implementation of functions called by compiler instrumentation
@@ -60,6 +81,12 @@ void
 __pat_tp_func_entry( void* func,
                      void* callsite )
 {
+    if ( scorep_first_func == NULL )
+    {
+        scorep_first_func     = func;
+        scorep_first_callsite = callsite;
+        SCOREP_RegisterExitCallback( &scorep_compiler_cray_final_exit );
+    }
     __cyg_profile_func_enter( func, callsite );
 }
 
@@ -74,4 +101,8 @@ __pat_tp_func_return( void* func,
                       void* callsite )
 {
     __cyg_profile_func_exit( func, callsite );
+    if ( func == scorep_first_func )
+    {
+        scorep_exited_first_func = true;
+    }
 }
