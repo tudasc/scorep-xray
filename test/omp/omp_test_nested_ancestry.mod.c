e5f206d633492b543edf6a34e83b4166aacd9bbc
Peter Philippen <p.philippen@fz-juelich.de>
2013-11-11 13:51:08 +0000

    Removed references to local file structure in preinstrumented test files
    Subversion: revision:5490

--- test/omp/omp_test_nested_ancestry.mod.c
+++ test/omp/omp_test_nested_ancestry.mod.c
@@ -1,7 +1,7 @@
 /* *INDENT-OFF* */
 #include <config.h>
 #include "omp_test_nested_ancestry.c.opari.inc"
-#line 1 "/private/peterp/work/scorep/TRY_RB_1.3_PPHILIPPEN_omp_ancestry/test/omp/omp_test_nested_ancestry.c"
+#line 1 "omp_test_nested_ancestry.c"
 /*
  * This file is part of the Score-P software (http://www.score-p.org)
  *
@@ -45,7 +45,7 @@
 #define N 10
 
 int outer, middle;
-#line 45 "/private/peterp/work/scorep/TRY_RB_1.3_PPHILIPPEN_omp_ancestry/test/omp/omp_test_nested_ancestry.c"
+#line 45 "omp_test_nested_ancestry.c"
 #pragma omp threadprivate(outer, middle)
 
 float foo(int n)
@@ -81,10 +81,10 @@ int main(int argc, char* argv[]){
   int pomp2_if = 1;
   POMP2_Task_handle pomp2_old_task;
   POMP2_Parallel_fork(&pomp2_region_1, pomp2_if, pomp2_num_threads, &pomp2_old_task, pomp2_ctc_1 );
-#line 75 "/private/peterp/work/scorep/TRY_RB_1.3_PPHILIPPEN_omp_ancestry/test/omp/omp_test_nested_ancestry.c"
+#line 75 "omp_test_nested_ancestry.c"
 #pragma omp parallel                POMP2_DLIST_00001 firstprivate(pomp2_old_task) num_threads(pomp2_num_threads)
 {   POMP2_Parallel_begin( &pomp2_region_1 );
-#line 76 "/private/peterp/work/scorep/TRY_RB_1.3_PPHILIPPEN_omp_ancestry/test/omp/omp_test_nested_ancestry.c"
+#line 76 "omp_test_nested_ancestry.c"
       {
         outer = omp_get_thread_num();
 {
@@ -92,10 +92,10 @@ int main(int argc, char* argv[]){
   int pomp2_if = 1;
   POMP2_Task_handle pomp2_old_task;
   POMP2_Parallel_fork(&pomp2_region_2, pomp2_if, pomp2_num_threads, &pomp2_old_task, pomp2_ctc_2 );
-#line 78 "/private/peterp/work/scorep/TRY_RB_1.3_PPHILIPPEN_omp_ancestry/test/omp/omp_test_nested_ancestry.c"
+#line 78 "omp_test_nested_ancestry.c"
 #pragma omp parallel                copyin(outer) POMP2_DLIST_00002 firstprivate(pomp2_old_task) num_threads(pomp2_num_threads)
 {   POMP2_Parallel_begin( &pomp2_region_2 );
-#line 79 "/private/peterp/work/scorep/TRY_RB_1.3_PPHILIPPEN_omp_ancestry/test/omp/omp_test_nested_ancestry.c"
+#line 79 "omp_test_nested_ancestry.c"
         {
           middle = omp_get_thread_num();
 {
@@ -103,18 +103,18 @@ int main(int argc, char* argv[]){
   int pomp2_if = 1;
   POMP2_Task_handle pomp2_old_task;
   POMP2_Parallel_fork(&pomp2_region_3, pomp2_if, pomp2_num_threads, &pomp2_old_task, pomp2_ctc_3 );
-#line 81 "/private/peterp/work/scorep/TRY_RB_1.3_PPHILIPPEN_omp_ancestry/test/omp/omp_test_nested_ancestry.c"
+#line 81 "omp_test_nested_ancestry.c"
 #pragma omp parallel                copyin(outer, middle) POMP2_DLIST_00003 firstprivate(pomp2_old_task) num_threads(pomp2_num_threads)
 {   POMP2_Parallel_begin( &pomp2_region_3 );
-#line 82 "/private/peterp/work/scorep/TRY_RB_1.3_PPHILIPPEN_omp_ancestry/test/omp/omp_test_nested_ancestry.c"
+#line 82 "omp_test_nested_ancestry.c"
           {
 {   POMP2_Atomic_enter( &pomp2_region_4, pomp2_ctc_4  );
-#line 83 "/private/peterp/work/scorep/TRY_RB_1.3_PPHILIPPEN_omp_ancestry/test/omp/omp_test_nested_ancestry.c"
+#line 83 "omp_test_nested_ancestry.c"
 #pragma omp atomic
             sum += foo(i);
   POMP2_Atomic_exit( &pomp2_region_4 );
  }
-#line 85 "/private/peterp/work/scorep/TRY_RB_1.3_PPHILIPPEN_omp_ancestry/test/omp/omp_test_nested_ancestry.c"
+#line 85 "omp_test_nested_ancestry.c"
           }
 { POMP2_Task_handle pomp2_old_task;
   POMP2_Implicit_barrier_enter( &pomp2_region_3, &pomp2_old_task );
@@ -122,7 +122,7 @@ int main(int argc, char* argv[]){
   POMP2_Implicit_barrier_exit( &pomp2_region_3, pomp2_old_task ); }
   POMP2_Parallel_end( &pomp2_region_3 ); }
   POMP2_Parallel_join( &pomp2_region_3, pomp2_old_task ); }
-#line 86 "/private/peterp/work/scorep/TRY_RB_1.3_PPHILIPPEN_omp_ancestry/test/omp/omp_test_nested_ancestry.c"
+#line 86 "omp_test_nested_ancestry.c"
         }
 { POMP2_Task_handle pomp2_old_task;
   POMP2_Implicit_barrier_enter( &pomp2_region_2, &pomp2_old_task );
@@ -130,7 +130,7 @@ int main(int argc, char* argv[]){
   POMP2_Implicit_barrier_exit( &pomp2_region_2, pomp2_old_task ); }
   POMP2_Parallel_end( &pomp2_region_2 ); }
   POMP2_Parallel_join( &pomp2_region_2, pomp2_old_task ); }
-#line 87 "/private/peterp/work/scorep/TRY_RB_1.3_PPHILIPPEN_omp_ancestry/test/omp/omp_test_nested_ancestry.c"
+#line 87 "omp_test_nested_ancestry.c"
       }
 { POMP2_Task_handle pomp2_old_task;
   POMP2_Implicit_barrier_enter( &pomp2_region_1, &pomp2_old_task );
@@ -138,7 +138,7 @@ int main(int argc, char* argv[]){
   POMP2_Implicit_barrier_exit( &pomp2_region_1, pomp2_old_task ); }
   POMP2_Parallel_end( &pomp2_region_1 ); }
   POMP2_Parallel_join( &pomp2_region_1, pomp2_old_task ); }
-#line 88 "/private/peterp/work/scorep/TRY_RB_1.3_PPHILIPPEN_omp_ancestry/test/omp/omp_test_nested_ancestry.c"
+#line 88 "omp_test_nested_ancestry.c"
     }
   printf("Sum=%f\n", sum);
 
@@ -150,10 +150,10 @@ int main(int argc, char* argv[]){
   int pomp2_if = 1;
   POMP2_Task_handle pomp2_old_task;
   POMP2_Parallel_fork(&pomp2_region_5, pomp2_if, pomp2_num_threads, &pomp2_old_task, pomp2_ctc_5 );
-#line 94 "/private/peterp/work/scorep/TRY_RB_1.3_PPHILIPPEN_omp_ancestry/test/omp/omp_test_nested_ancestry.c"
+#line 94 "omp_test_nested_ancestry.c"
 #pragma omp parallel                POMP2_DLIST_00005 firstprivate(pomp2_old_task) num_threads(pomp2_num_threads)
 {   POMP2_Parallel_begin( &pomp2_region_5 );
-#line 95 "/private/peterp/work/scorep/TRY_RB_1.3_PPHILIPPEN_omp_ancestry/test/omp/omp_test_nested_ancestry.c"
+#line 95 "omp_test_nested_ancestry.c"
       {
         outer = omp_get_thread_num();
 {
@@ -161,19 +161,19 @@ int main(int argc, char* argv[]){
   int pomp2_if = 1;
   POMP2_Task_handle pomp2_old_task;
   POMP2_Parallel_fork(&pomp2_region_6, pomp2_if, pomp2_num_threads, &pomp2_old_task, pomp2_ctc_6 );
-#line 97 "/private/peterp/work/scorep/TRY_RB_1.3_PPHILIPPEN_omp_ancestry/test/omp/omp_test_nested_ancestry.c"
+#line 97 "omp_test_nested_ancestry.c"
 #pragma omp parallel                copyin(outer) POMP2_DLIST_00006 firstprivate(pomp2_old_task) num_threads(pomp2_num_threads)
 {   POMP2_Parallel_begin( &pomp2_region_6 );
-#line 98 "/private/peterp/work/scorep/TRY_RB_1.3_PPHILIPPEN_omp_ancestry/test/omp/omp_test_nested_ancestry.c"
+#line 98 "omp_test_nested_ancestry.c"
         {
           middle = omp_get_thread_num();
 {   POMP2_Atomic_enter( &pomp2_region_7, pomp2_ctc_7  );
-#line 100 "/private/peterp/work/scorep/TRY_RB_1.3_PPHILIPPEN_omp_ancestry/test/omp/omp_test_nested_ancestry.c"
+#line 100 "omp_test_nested_ancestry.c"
 #pragma omp atomic
           sum += foo(i);
   POMP2_Atomic_exit( &pomp2_region_7 );
  }
-#line 102 "/private/peterp/work/scorep/TRY_RB_1.3_PPHILIPPEN_omp_ancestry/test/omp/omp_test_nested_ancestry.c"
+#line 102 "omp_test_nested_ancestry.c"
         }
 { POMP2_Task_handle pomp2_old_task;
   POMP2_Implicit_barrier_enter( &pomp2_region_6, &pomp2_old_task );
@@ -181,7 +181,7 @@ int main(int argc, char* argv[]){
   POMP2_Implicit_barrier_exit( &pomp2_region_6, pomp2_old_task ); }
   POMP2_Parallel_end( &pomp2_region_6 ); }
   POMP2_Parallel_join( &pomp2_region_6, pomp2_old_task ); }
-#line 103 "/private/peterp/work/scorep/TRY_RB_1.3_PPHILIPPEN_omp_ancestry/test/omp/omp_test_nested_ancestry.c"
+#line 103 "omp_test_nested_ancestry.c"
       }
 { POMP2_Task_handle pomp2_old_task;
   POMP2_Implicit_barrier_enter( &pomp2_region_5, &pomp2_old_task );
@@ -189,7 +189,7 @@ int main(int argc, char* argv[]){
   POMP2_Implicit_barrier_exit( &pomp2_region_5, pomp2_old_task ); }
   POMP2_Parallel_end( &pomp2_region_5 ); }
   POMP2_Parallel_join( &pomp2_region_5, pomp2_old_task ); }
-#line 104 "/private/peterp/work/scorep/TRY_RB_1.3_PPHILIPPEN_omp_ancestry/test/omp/omp_test_nested_ancestry.c"
+#line 104 "omp_test_nested_ancestry.c"
     }
   printf("Sum=%f\n", sum);
 
@@ -201,10 +201,10 @@ int main(int argc, char* argv[]){
   int pomp2_if = 1;
   POMP2_Task_handle pomp2_old_task;
   POMP2_Parallel_fork(&pomp2_region_8, pomp2_if, pomp2_num_threads, &pomp2_old_task, pomp2_ctc_8 );
-#line 110 "/private/peterp/work/scorep/TRY_RB_1.3_PPHILIPPEN_omp_ancestry/test/omp/omp_test_nested_ancestry.c"
+#line 110 "omp_test_nested_ancestry.c"
 #pragma omp parallel                POMP2_DLIST_00008 firstprivate(pomp2_old_task) num_threads(pomp2_num_threads)
 {   POMP2_Parallel_begin( &pomp2_region_8 );
-#line 111 "/private/peterp/work/scorep/TRY_RB_1.3_PPHILIPPEN_omp_ancestry/test/omp/omp_test_nested_ancestry.c"
+#line 111 "omp_test_nested_ancestry.c"
       {
         outer = omp_get_thread_num();
 {
@@ -212,18 +212,18 @@ int main(int argc, char* argv[]){
   int pomp2_if = 1;
   POMP2_Task_handle pomp2_old_task;
   POMP2_Parallel_fork(&pomp2_region_9, pomp2_if, pomp2_num_threads, &pomp2_old_task, pomp2_ctc_9 );
-#line 113 "/private/peterp/work/scorep/TRY_RB_1.3_PPHILIPPEN_omp_ancestry/test/omp/omp_test_nested_ancestry.c"
+#line 113 "omp_test_nested_ancestry.c"
 #pragma omp parallel                copyin(outer) POMP2_DLIST_00009 firstprivate(pomp2_old_task) num_threads(pomp2_num_threads)
 {   POMP2_Parallel_begin( &pomp2_region_9 );
-#line 114 "/private/peterp/work/scorep/TRY_RB_1.3_PPHILIPPEN_omp_ancestry/test/omp/omp_test_nested_ancestry.c"
+#line 114 "omp_test_nested_ancestry.c"
         {
 {   POMP2_Atomic_enter( &pomp2_region_10, pomp2_ctc_10  );
-#line 115 "/private/peterp/work/scorep/TRY_RB_1.3_PPHILIPPEN_omp_ancestry/test/omp/omp_test_nested_ancestry.c"
+#line 115 "omp_test_nested_ancestry.c"
 #pragma omp atomic
           sum += foo(i);
   POMP2_Atomic_exit( &pomp2_region_10 );
  }
-#line 117 "/private/peterp/work/scorep/TRY_RB_1.3_PPHILIPPEN_omp_ancestry/test/omp/omp_test_nested_ancestry.c"
+#line 117 "omp_test_nested_ancestry.c"
         }
 { POMP2_Task_handle pomp2_old_task;
   POMP2_Implicit_barrier_enter( &pomp2_region_9, &pomp2_old_task );
@@ -231,7 +231,7 @@ int main(int argc, char* argv[]){
   POMP2_Implicit_barrier_exit( &pomp2_region_9, pomp2_old_task ); }
   POMP2_Parallel_end( &pomp2_region_9 ); }
   POMP2_Parallel_join( &pomp2_region_9, pomp2_old_task ); }
-#line 118 "/private/peterp/work/scorep/TRY_RB_1.3_PPHILIPPEN_omp_ancestry/test/omp/omp_test_nested_ancestry.c"
+#line 118 "omp_test_nested_ancestry.c"
       }
 { POMP2_Task_handle pomp2_old_task;
   POMP2_Implicit_barrier_enter( &pomp2_region_8, &pomp2_old_task );
@@ -239,7 +239,7 @@ int main(int argc, char* argv[]){
   POMP2_Implicit_barrier_exit( &pomp2_region_8, pomp2_old_task ); }
   POMP2_Parallel_end( &pomp2_region_8 ); }
   POMP2_Parallel_join( &pomp2_region_8, pomp2_old_task ); }
-#line 119 "/private/peterp/work/scorep/TRY_RB_1.3_PPHILIPPEN_omp_ancestry/test/omp/omp_test_nested_ancestry.c"
+#line 119 "omp_test_nested_ancestry.c"
     }
 
   printf("Sum=%f\n", sum);
