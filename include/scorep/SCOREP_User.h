416ac7b3f4650e7db171ba7ea089636e611a3f1a
Bert Wesarg <bert.wesarg@tu-dresden.de>
2014-04-25 19:34:09 +0000

    Bring back copyright year. Got lost in revision:6376.
    Subversion: revision:6465

--- include/scorep/SCOREP_User.h
+++ include/scorep/SCOREP_User.h
@@ -7,7 +7,7 @@
  * Copyright (c) 2009-2011,
  * Gesellschaft fuer numerische Simulation mbH Braunschweig, Germany
  *
- * Copyright (c) 2009-2011,
+ * Copyright (c) 2009-2011, 2014,
  * Technische Universitaet Dresden, Germany
  *
  * Copyright (c) 2009-2011,
