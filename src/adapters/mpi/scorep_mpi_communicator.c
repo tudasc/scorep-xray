0d848a11a29a06b7d1d008b5c9b8d07fcd155e78
Bert Wesarg <bert.wesarg@tu-dresden.de>
2014-04-29 11:10:17 +0000

    Fix conditions to select not the deprecated functions.
    Subversion: revision:6496

--- src/adapters/mpi/scorep_mpi_communicator.c
+++ src/adapters/mpi/scorep_mpi_communicator.c
@@ -308,7 +308,7 @@ scorep_mpi_setup_world( void )
 
     /* create a derived datatype for distributed communicator
      * definition handling */
-#if HAVE( MPI_GET_ADDRESS )
+#if HAVE( DECL_MPI_GET_ADDRESS )
     PMPI_Get_address( &pair.id, &( disp[ 0 ] ) );
     PMPI_Get_address( &pair.root, &( disp[ 1 ] ) );
 #else
@@ -320,7 +320,7 @@ scorep_mpi_setup_world( void )
         disp[ i ] -= disp[ 0 ];
     }
 
-#if HAVE( MPI_TYPE_CREATE_STRUCT )
+#if HAVE( DECL_MPI_TYPE_CREATE_STRUCT )
     PMPI_Type_create_struct( 2, lengths, disp, types, &scorep_mpi_id_root_type );
 #else
     PMPI_Type_struct( 2, lengths, disp, types, &scorep_mpi_id_root_type );
