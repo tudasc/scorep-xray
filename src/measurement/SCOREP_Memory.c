60c643e1dafbca91446a75bb9bddaee0be1c5948
Bert Wesarg <bert.wesarg@tu-dresden.de>
2014-07-16 13:12:31 +0000

    Remove `scorep_` prefix from static variables.
    Subversion: revision:7037

--- src/measurement/SCOREP_Memory.c
+++ src/measurement/SCOREP_Memory.c
@@ -7,7 +7,7 @@
  * Copyright (c) 2009-2012,
  * Gesellschaft fuer numerische Simulation mbH Braunschweig, Germany
  *
- * Copyright (c) 2009-2012,
+ * Copyright (c) 2009-2012, 2014,
  * Technische Universitaet Dresden, Germany
  *
  * Copyright (c) 2009-2012,
@@ -60,12 +60,12 @@ static SCOREP_Mutex memory_lock;
 
 
 /// The one and only allocator for the measurement and the adapters
-static SCOREP_Allocator_Allocator* scorep_memory_allocator = 0;
-static uint64_t                    scorep_memory_total_memory;
+static SCOREP_Allocator_Allocator* allocator;
+static size_t                      total_memory;
 
-static bool scorep_memory_is_initialized = false;
+static bool is_initialized;
 
-static SCOREP_Allocator_PageManager* scorep_definitions_page_manager;
+static SCOREP_Allocator_PageManager* definitions_page_manager;
 
 void
 SCOREP_Memory_Initialize( size_t totalMemory,
@@ -73,52 +73,52 @@ SCOREP_Memory_Initialize( size_t totalMemory,
 {
     assert( totalMemory >= pageSize );
 
-    if ( scorep_memory_is_initialized )
+    if ( is_initialized )
     {
         return;
     }
-    scorep_memory_is_initialized = true;
+    is_initialized = true;
 
     SCOREP_MutexCreate( &memory_lock );
 
-    assert( scorep_memory_allocator == 0 );
-    scorep_memory_allocator = SCOREP_Allocator_CreateAllocator(
+    assert( allocator == 0 );
+    allocator = SCOREP_Allocator_CreateAllocator(
         totalMemory,
         pageSize,
         ( SCOREP_Allocator_Guard )SCOREP_MutexLock,
         ( SCOREP_Allocator_Guard )SCOREP_MutexUnlock,
         ( SCOREP_Allocator_GuardObject )memory_lock );
 
-    if ( !scorep_memory_allocator )
+    if ( !allocator )
     {
         SCOREP_MutexDestroy( &memory_lock );
-        scorep_memory_is_initialized = false;
+        is_initialized = false;
         assert( false );
     }
 
-    assert( scorep_definitions_page_manager == 0 );
-    scorep_definitions_page_manager = SCOREP_Allocator_CreatePageManager( scorep_memory_allocator );
-    if ( !scorep_definitions_page_manager )
+    assert( definitions_page_manager == 0 );
+    definitions_page_manager = SCOREP_Allocator_CreatePageManager( allocator );
+    if ( !definitions_page_manager )
     {
         SCOREP_MutexDestroy( &memory_lock );
-        scorep_memory_is_initialized = false;
+        is_initialized = false;
         SCOREP_Memory_HandleOutOfMemory();
     }
 
-    scorep_memory_total_memory = totalMemory;
+    total_memory = totalMemory;
 }
 
 
 void
-SCOREP_Memory_Finalize()
+SCOREP_Memory_Finalize( void )
 {
-    if ( !scorep_memory_is_initialized )
+    if ( !is_initialized )
     {
         return;
     }
-    scorep_memory_is_initialized = false;
+    is_initialized = false;
 
-    assert( scorep_definitions_page_manager );
+    assert( definitions_page_manager );
 #if defined ( __INTEL_COMPILER ) && ( __INTEL_COMPILER < 1120 )
     // Do nothing here. Intel OpenMP RTL shuts down at the end of main
     // function, so omp_set/unset_lock, which is called after the end
@@ -126,13 +126,13 @@ SCOREP_Memory_Finalize()
     // problem will be fixed in  Intel Compiler 11.1 update 6.
     // See http://software.intel.com/en-us/forums/showpost.php?p=110592
 #else
-    SCOREP_Allocator_DeletePageManager( scorep_definitions_page_manager );
+    SCOREP_Allocator_DeletePageManager( definitions_page_manager );
 #endif
-    scorep_definitions_page_manager = 0;
+    definitions_page_manager = 0;
 
-    assert( scorep_memory_allocator );
-    SCOREP_Allocator_DeleteAllocator( scorep_memory_allocator );
-    scorep_memory_allocator = 0;
+    assert( allocator );
+    SCOREP_Allocator_DeleteAllocator( allocator );
+    allocator = 0;
 
     SCOREP_MutexDestroy( &memory_lock );
 }
@@ -141,8 +141,8 @@ void
 SCOREP_Memory_HandleOutOfMemory( void )
 {
     UTILS_ERROR( SCOREP_ERROR_MEMORY_OUT_OF_PAGES,
-                 "Out of memory. Please increase SCOREP_TOTAL_MEMORY=%" PRIu64 " and try again.",
-                 scorep_memory_total_memory );
+                 "Out of memory. Please increase SCOREP_TOTAL_MEMORY=%zu and try again.",
+                 total_memory );
     if ( SCOREP_Env_DoTracing() )
     {
         UTILS_ERROR( SCOREP_ERROR_MEMORY_OUT_OF_PAGES,
@@ -162,7 +162,7 @@ SCOREP_Allocator_PageManager*
 SCOREP_Memory_CreateTracingPageManager( void )
 {
     SCOREP_Allocator_PageManager* page_manager =
-        SCOREP_Allocator_CreatePageManager( scorep_memory_allocator );
+        SCOREP_Allocator_CreatePageManager( allocator );
     if ( !page_manager )
     {
         /* aborts */
@@ -183,7 +183,7 @@ SCOREP_Memory_CreatePageManagers( SCOREP_Allocator_PageManager** pageManagers )
             continue;
         }
         pageManagers[ i ] =
-            SCOREP_Allocator_CreatePageManager( scorep_memory_allocator );
+            SCOREP_Allocator_CreatePageManager( allocator );
         if ( !pageManagers[ i ] )
         {
             /* aborts */
@@ -254,7 +254,7 @@ free_memory_type_for_location( SCOREP_Location* location,
 }
 
 void
-SCOREP_Memory_FreeMiscMem()
+SCOREP_Memory_FreeMiscMem( void )
 {
     // print mem usage statistics
     SCOREP_MemoryType type = SCOREP_MEMORY_TYPE_MISC;
@@ -285,7 +285,7 @@ SCOREP_Location_AllocForProfile( SCOREP_Location* location, size_t size )
 
 
 void
-SCOREP_Memory_FreeProfileMem()
+SCOREP_Memory_FreeProfileMem( void )
 {
     // print mem usage statistics
     SCOREP_MemoryType type = SCOREP_MEMORY_TYPE_PROFILING;
@@ -303,7 +303,7 @@ SCOREP_Memory_AllocForDefinitions( SCOREP_Location* location,
         return SCOREP_MOVABLE_NULL;
     }
 
-    SCOREP_Allocator_PageManager* page_manager = scorep_definitions_page_manager;
+    SCOREP_Allocator_PageManager* page_manager = definitions_page_manager;
     if ( location )
     {
         page_manager = SCOREP_Location_GetMemoryPageManager(
@@ -323,10 +323,10 @@ SCOREP_Memory_AllocForDefinitions( SCOREP_Location* location,
 
 
 void
-SCOREP_Memory_FreeDefinitionMem()
+SCOREP_Memory_FreeDefinitionMem( void )
 {
     // print mem usage statistics
-    SCOREP_Allocator_Free( scorep_definitions_page_manager );
+    SCOREP_Allocator_Free( definitions_page_manager );
 }
 
 
@@ -343,12 +343,12 @@ SCOREP_Memory_GetAddressFromMovableMemory( SCOREP_Allocator_MovableMemory movabl
 SCOREP_Allocator_PageManager*
 SCOREP_Memory_CreateMovedPagedMemory( void )
 {
-    return SCOREP_Allocator_CreateMovedPageManager( scorep_memory_allocator );
+    return SCOREP_Allocator_CreateMovedPageManager( allocator );
 }
 
 SCOREP_Allocator_PageManager*
-SCOREP_Memory_GetLocalDefinitionPageManager()
+SCOREP_Memory_GetLocalDefinitionPageManager( void )
 {
-    assert( scorep_memory_is_initialized );
-    return scorep_definitions_page_manager;
+    assert( is_initialized );
+    return definitions_page_manager;
 }
