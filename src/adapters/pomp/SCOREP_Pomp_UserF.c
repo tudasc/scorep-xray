6511a96491c11601f50060c9daa087ea3fed415b
Ronny Tschüter <ronny.tschueter@tu-dresden.de>
2014-11-25 08:27:19 +0000

    Remove 'INDENT OFF' remark and beautify code
    Subversion: revision:7796

--- src/adapters/pomp/SCOREP_Pomp_UserF.c
+++ src/adapters/pomp/SCOREP_Pomp_UserF.c
@@ -42,41 +42,46 @@
 /*
  * Fortran wrappers calling the C versions
  */
-/* *INDENT-OFF*  */
 
-void FSUB(POMP2_Begin)( POMP2_Region_handle_fortran* regionHandle,
-                        const char*                  ctc_string )
+void
+FSUB( POMP2_Begin )( POMP2_Region_handle_fortran* regionHandle,
+                     const char*                  ctc_string )
 {
     if ( scorep_pomp_is_tracing_on )
     {
-        POMP2_Begin(SCOREP_POMP_F2C_REGION( regionHandle ), ctc_string );
+        POMP2_Begin( SCOREP_POMP_F2C_REGION( regionHandle ), ctc_string );
     }
 }
 
-void FSUB(POMP2_End)( POMP2_Region_handle_fortran* regionHandle )
+void
+FSUB( POMP2_End )( POMP2_Region_handle_fortran* regionHandle )
 {
     if ( scorep_pomp_is_tracing_on )
     {
-        POMP2_End(SCOREP_POMP_F2C_REGION( regionHandle ));
+        POMP2_End( SCOREP_POMP_F2C_REGION( regionHandle ) );
     }
 }
 
-void FSUB(POMP2_Init)( void )
+void
+FSUB( POMP2_Init )( void )
 {
     POMP2_Init();
 }
 
-void FSUB(POMP2_Finalize)( void )
+void
+FSUB( POMP2_Finalize )( void )
 {
     POMP2_Finalize();
 }
 
-void FSUB(POMP2_On)( void )
+void
+FSUB( POMP2_On )( void )
 {
     scorep_pomp_is_tracing_on = 1;
 }
 
-void FSUB(POMP2_Off)( void )
+void
+FSUB( POMP2_Off )( void )
 {
     scorep_pomp_is_tracing_on = 0;
 }
