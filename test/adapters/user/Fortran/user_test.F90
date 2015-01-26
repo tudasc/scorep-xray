71a7bc15c7f093632ce7111cd517eaa177f8b4c9
Bert Wesarg <bert.wesarg@tu-dresden.de>
2015-01-26 14:03:16 +0000

    Update copyright header.
    Subversion: revision:8167

--- test/adapters/user/Fortran/user_test.F90
+++ test/adapters/user/Fortran/user_test.F90
@@ -1,15 +1,30 @@
 !
-! This file is part of the SCOREP project (http:
+!  This file is part of the Score-P software (http://www.score-p.org)
 !
-! Copyright (c) 2009-2011,
-! RWTH Aachen, Germany
-! Gesellschaft fuer numerische Simulation mbH Braunschweig, Germany
-! Technische Universitaet Dresden, Germany
-! University of Oregon, Eugene USA
-! Forschungszentrum Juelich GmbH, Germany
-! Technische Universitaet Muenchen, Germany
+!  Copyright (c) 2009-2011,
+!  RWTH Aachen University, Germany
 !
-! See the COPYING file in the package base directory for details.
+!  Copyright (c) 2009-2011,
+!  Gesellschaft fuer numerische Simulation mbH Braunschweig, Germany
+!
+!  Copyright (c) 2009-2011, 2015,
+!  Technische Universitaet Dresden, Germany
+!
+!  Copyright (c) 2009-2011,
+!  University of Oregon, Eugene, USA
+!
+!  Copyright (c) 2009-2011,
+!  Forschungszentrum Juelich GmbH, Germany
+!
+!  Copyright (c) 2009-2011,
+!  German Research School for Simulation Sciences GmbH, Juelich/Aachen, Germany
+!
+!  Copyright (c) 2009-2011,
+!  Technische Universitaet Muenchen, Germany
+!
+!  This software may be modified and distributed under the terms of
+!  a BSD-style license.  See the COPYING file in the package base
+!  directory for details.
 !
 
 #include <scorep/SCOREP_User.inc>
