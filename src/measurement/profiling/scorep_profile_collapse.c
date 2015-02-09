53a25f4f9301fb0691ce3f2bc2e686becbf1ddb4
Daniel Lorenz <lorenz@cs.tu-darmstadt.de>
2015-02-09 09:34:04 +0000

    Added Changelog entry and copyright update for last change. See ticket:940
    Subversion: revision:8362

--- src/measurement/profiling/scorep_profile_collapse.c
+++ src/measurement/profiling/scorep_profile_collapse.c
@@ -22,6 +22,9 @@
  * Copyright (c) 2009-2012,
  * Technische Universitaet Muenchen, Germany
  *
+ * Copyright (c) 2015,
+ * Technische Universitaet Darmstadt, Germany
+ *
  * This software may be modified and distributed under the terms of
  * a BSD-style license. See the COPYING file in the package base
  * directory for details.
