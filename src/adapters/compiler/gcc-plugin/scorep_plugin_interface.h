0deff533bc0f03a7f22fe6854d5e7a3d81d5ca33
Thomas Ilsche <thomas.ilsche@tu-dresden.de>
2016-03-04 22:12:50 +0000

    Add missing copyright header.
    Subversion: revision:10090

--- src/adapters/compiler/gcc-plugin/scorep_plugin_interface.h
+++ src/adapters/compiler/gcc-plugin/scorep_plugin_interface.h
@@ -1,3 +1,22 @@
+/*
+ * This file is part of the Score-P software (http://www.score-p.org)
+ *
+ * Copyright (c) 2016,
+ * Technische Universitaet Dresden, Germany
+ *
+ * This software may be modified and distributed under the terms of
+ * a BSD-style license.  See the COPYING file in the package base
+ * directory for details.
+ *
+ */
+
+/**
+ * @file
+ *
+ * @brief  A wrapper for gcc-plugin.h.
+ *
+ */
+
 #ifndef SCOREP_PLUGIN_INTERFACE_H
 #define SCOREP_PLUGIN_INTERFACE_H
 
