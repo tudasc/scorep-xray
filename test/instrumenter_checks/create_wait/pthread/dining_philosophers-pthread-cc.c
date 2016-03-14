22370fa9705c927538ecab2c251b79716d2cd379
Thomas Ilsche <thomas.ilsche@tu-dresden.de>
2016-03-14 09:09:04 +0000

    Add missing copyright header.
    Subversion: revision:10135

--- test/instrumenter_checks/create_wait/pthread/dining_philosophers-pthread-cc.c
+++ test/instrumenter_checks/create_wait/pthread/dining_philosophers-pthread-cc.c
@@ -1,15 +1,17 @@
 /*
-** This file is part of the Score-P software (http://www.score-p.org)
-**
-** Copyright (c) 2014,2016
-** Forschungszentrum Juelich GmbH, Germany
-** Technische Universitaet Dresden, Germany
-**
-** This software may be modified and distributed under the terms of
-** a BSD-style license.  See the COPYING file in the package base
-** directory for details.
-**
-**/
+ * This file is part of the Score-P software (http://www.score-p.org)
+ *
+ * Copyright (c) 2014
+ * Forschungszentrum Juelich GmbH, Germany
+ *
+ * Copyright (c) 2016
+ * Technische Universitaet Dresden, Germany
+ *
+ * This software may be modified and distributed under the terms of
+ * a BSD-style license.  See the COPYING file in the package base
+ * directory for details.
+ *
+ */
 
 
 /**
