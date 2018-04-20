320bf1ba192b677bf417c38dacd46f45630dac3c
Bert Wesarg <bert.wesarg@tu-dresden.de>
2018-04-20 08:39:30 +0000

    Let the GCC plug-in compile with very old C++ compilers.
    Subversion: revision:13427

--- src/adapters/compiler/gcc-plugin/scorep_plugin_attribute.inc.c
+++ src/adapters/compiler/gcc-plugin/scorep_plugin_attribute.inc.c
@@ -32,19 +32,19 @@
 
 struct attribute_spec scorep_no_instrument_attribute =
 {
-    .name                   = "scorep_no_instrument",
-    .min_length             = 0,
-    .max_length             = 0,
-    .decl_required          = false,
-    .type_required          = false,
-    .function_type_required = false,
+    /* .name                   = */ "scorep_no_instrument",
+    /* .min_length             = */ 0,
+    /* .max_length             = */ 0,
+    /* .decl_required          = */ false,
+    /* .type_required          = */ false,
+    /* .function_type_required = */ false,
     #if SCOREP_GCC_PLUGIN_TARGET_VERSION >= 8000
-    .affects_type_identity = false,
-    .handler               = NULL
+    /* .affects_type_identity = */ false,
+    /* .handler               = */ NULL
     #else
-    .handler                = NULL,
+    /* .handler                = */ NULL,
     #if SCOREP_GCC_PLUGIN_TARGET_VERSION >= 4007
-    .affects_type_identity  = false
+    /* .affects_type_identity  = */ false
     #endif
     #endif
 };
