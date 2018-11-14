4ca10b7db728d31b1661e5e6a8391da67c675655
Christian Feld <c.feld@fz-juelich.de>
2018-11-14 07:45:30 +0000

    Add missing omp_tasks_untied.pomp.c, see revision:13884.
    Subversion: revision:13885

---
+++ test/omp_tasks/omp_tasks_untied.pomp.c
@@ -0,0 +1,26 @@
+#include <config.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+#include <stddef.h>
+
+extern void
+FORTRAN_MANGLED( pomp2_init_reg_sjwin65ixa08_3 )();
+
+void
+POMP2_Init_regions()
+{
+    FORTRAN_MANGLED( pomp2_init_reg_sjwin65ixa08_3 )();
+}
+
+size_t
+POMP2_Get_num_regions()
+{
+    return 4;
+}
+
+#ifdef __cplusplus
+}
+#endif
