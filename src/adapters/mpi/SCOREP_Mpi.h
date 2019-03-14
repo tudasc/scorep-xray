44505ef282173aa3b5a63a16bcee5507044a3f8f
Bert Wesarg <bert.wesarg@tu-dresden.de>
2019-03-14 13:04:58 +0000

    Avoid common symbol
    Subversion: revision:14327

--- src/adapters/mpi/SCOREP_Mpi.h
+++ src/adapters/mpi/SCOREP_Mpi.h
@@ -121,7 +121,7 @@ extern bool scorep_mpi_generate_events;
 /**
    Flag which indicates whether recording of MPI topologies is enabled.
  */
-bool scorep_mpi_enable_topologies;
+extern bool scorep_mpi_enable_topologies;
 
 /** @def SCOREP_MPI_IS_EVENT_GEN_ON
     Check whether event generation is turned on.
