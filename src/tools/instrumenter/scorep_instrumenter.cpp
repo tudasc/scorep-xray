d13fb76f0807fd97919ddf64c4cedc59e66796bc
Christian Herold <christian.herold@tu-dresden.de>
2019-07-24 15:12:28 +0000

    Re-add start-group and end-group linker flags because they are required for POSIX runtime wrapping
    Subversion: revision:14651

--- src/tools/instrumenter/scorep_instrumenter.cpp
+++ src/tools/instrumenter/scorep_instrumenter.cpp
@@ -650,9 +650,18 @@ SCOREP_Instrumenter::link_step( void )
 #endif
     command << " `" << m_config_base << " --ldflags`";
     command << " " << m_command_line.getFlagsBeforeInterpositionLib();
+#if SCOREP_BACKEND_HAVE_LINKER_START_END_GROUP
+    command << libs_prefix << linker_prefix << "-start-group `" << m_config_base << " --event-libs`";
+    command << " " << m_command_line.getFlagsAfterInterpositionLib() << libs_suffix;
+#else
     command << libs_prefix << " `" << m_config_base << " --event-libs`" << libs_suffix;
     command << " " << m_command_line.getFlagsAfterInterpositionLib();
+#endif
+#if SCOREP_BACKEND_HAVE_LINKER_START_END_GROUP
+    command << libs_prefix << " `" << m_config_base << " --mgmt-libs`" << linker_prefix << "-end-group " << libs_suffix;
+#else
     command << libs_prefix << " `" << m_config_base << " --mgmt-libs`" << libs_suffix;
+#endif
     if ( m_command_line.getOutputName() != "" )
     {
         /* nvcc requires a space between -o and the output name. */
