1d9d7f04b5f74df582b6eaf237f59f89e332bfb2
Christian Feld <c.feld@fz-juelich.de>
2022-02-17 16:29:26 +0100

    Switch externals from rc3 to released cubew-4.7 and cubelib-4.7

--- vendor/externals.sh
+++ vendor/externals.sh
@@ -70,5 +70,5 @@ cmd=cmd_${1:-get}
 
 ${cmd} git otf2 -b v3.0 https://gitlab.jsc.fz-juelich.de/perftools/otf2.git
 ${cmd} git opari2 -b v2.0.6 https://gitlab.jsc.fz-juelich.de/perftools/opari2.git
-${cmd} git cubew -b v4.7-rc3 https://gitlab.jsc.fz-juelich.de/perftools/cubew.git
-${cmd} git cubelib -b v4.7-rc3 https://gitlab.jsc.fz-juelich.de/perftools/cubelib.git
+${cmd} git cubew -b v4.7 https://gitlab.jsc.fz-juelich.de/perftools/cubew.git
+${cmd} git cubelib -b v4.7 https://gitlab.jsc.fz-juelich.de/perftools/cubelib.git
