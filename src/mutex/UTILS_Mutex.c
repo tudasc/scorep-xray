0fa8574b9c0bc3a01db427a47a37912a3dd36e74
Markus Geimer <m.geimer@fz-juelich.de>
2022-05-11 16:08:26 +0200

    Extract the generic mutex implementation to common
    Make the generic mutex implementation a common cross-project
    functionality.  Rename the source files and convenience library,
    consistently use a `UTILS` prefix for preprocessor defines, types,
    and function names, and adjust all uses accordingly.

---
+++ src/mutex/UTILS_Mutex.c
@@ -0,0 +1,23 @@
+/*
+ * This file is part of the Score-P software ecosystem (http://www.score-p.org)
+ *
+ * Copyright (c) 2020, 2022,
+ * Forschungszentrum Juelich GmbH, Germany
+ *
+ * This software may be modified and distributed under the terms of
+ * a BSD-style license. See the COPYING file in the package base
+ * directory for details.
+ *
+ */
+
+
+#include <config.h>
+#include <UTILS_Mutex.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <UTILS_Atomic.h>
+#include <UTILS_Error.h>
+
+#define STATIC_INLINE
+#include "UTILS_Mutex.inc.c"
+#undef STATIC_INLINE
