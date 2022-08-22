877b74bb92bdd3eb4c0ee656244041ebc03b2754
Christian Feld <c.feld@fz-juelich.de>
2022-08-22 15:35:07 +0200

    PGI conditional compilation now language aware

--- src/tools/config/scorep_config_adapter.cpp
+++ src/tools/config/scorep_config_adapter.cpp
@@ -384,22 +384,16 @@ SCOREP_Config_CompilerAdapter::addLdFlags( std::string& ldflags,
     }
     else
     {
-        /*
-         * Add compiler instrumentation cflags, because we encountered a
-         * case on JUQUEEN with the XLC compiler, that performed
-         * rebuilding of code during linking on high optimization
-         * levels. If the link command does not contain compiler
-         * instrumentation flags, the code was not instrumented.
+        /* We used to add compiler instrumentation cflags here because the
+         * xlc compiler on BG/Q (deprecated) rebuild code at link time if ipo
+         * optimization levels were used. For PGI, adding flags failed as it then
+         * linked its own profiling libraries.
+         * Given that compiler instrumentation flags are now language dependent
+         * and calls are inserted at compile time, cflags should
+         * not be needed at link time.
          *
-         * However, we got errors with PGI on Todi, because compiler instrumentation
-         * functions were defined twice. Thus do not add the
-         * compiler instrumentation for PGI.
-         *
-         * See also ticket #855.
+         * See https://gitlab.jsc.fz-juelich.de/perftools-svntogit/scorep-trac/-/blob/master/ticket/855.tracwiki
          */
-#if !SCOREP_BACKEND_COMPILER_PGI
-        ldflags += " " SCOREP_COMPILER_INSTRUMENTATION_CFLAGS;
-#endif
         ldflags += " " SCOREP_COMPILER_INSTRUMENTATION_LDFLAGS;
     }
 }
