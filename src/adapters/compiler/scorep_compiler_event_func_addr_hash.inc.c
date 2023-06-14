41bfbf878ca88b6400428dc2148d36de6e67e384
Christian Feld <c.feld@fz-juelich.de>
2023-06-14 12:21:18 +0200

    Filter additional outlined OpenMP functions generated if target flags given
    For a list of tested options, see
    https://gitlab.jsc.fz-juelich.de/perftools/scorep/-/issues/256#note_189770

--- src/adapters/compiler/scorep_compiler_event_func_addr_hash.inc.c
+++ src/adapters/compiler/scorep_compiler_event_func_addr_hash.inc.c
@@ -115,6 +115,9 @@ func_addr_hash_value_ctor( func_addr_hash_key_t* addr,
 #if HAVE( SCOREP_COMPILER_CC_INTEL_ONEAPI ) || HAVE( SCOREP_COMPILER_CXX_INTEL_ONEAPI ) || HAVE( SCOREP_COMPILER_FC_INTEL_ONEAPI )
                        && ( !strstr( function_name_demangled, "_tree_reduce_" ) )
 #endif  /* Intel oneAPI compiler */
+                       && ( strncmp( function_name_demangled, "..omp", 5 ) != 0 )
+                       && ( strncmp( function_name_demangled, "__omp", 5 ) != 0 )
+                       && ( strncmp( function_name_demangled, "..acc", 5 ) != 0 )
                        && ( strncmp( function_name_demangled, ".nondebug_wrapper.", 18 ) != 0 )
                        && ( !strstr( function_name_demangled, "Kokkos::Tools" ) )
                        && ( !strstr( function_name_demangled, "Kokkos::Profiling" ) )
