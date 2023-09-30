tudasc/scorep-xray

commit 9c70d3e1298f7f37839e03e3eddbb81b66959f1f

Make definition getters static inliners

src/measurement/definitions/include/scorep_definitions_io_handle.h
@@ -87,17 +87,32 @@ void
 SCOREP_IoHandleHandle_SetIoFile( SCOREP_IoHandleHandle handle,
                                  SCOREP_IoFileHandle   file );
 
-SCOREP_IoFileHandle
-SCOREP_IoHandleHandle_GetIoFile( SCOREP_IoHandleHandle handle );
+static inline SCOREP_IoFileHandle
+SCOREP_IoHandleHandle_GetIoFile( SCOREP_IoHandleHandle handle )
+{
+    return SCOREP_LOCAL_HANDLE_DEREF( handle, IoHandle )->file_handle;
+}
 
-void*
-SCOREP_IoHandleHandle_GetPayload( SCOREP_IoHandleHandle handle );
+static inline void*
+SCOREP_IoHandleHandle_GetPayload( SCOREP_IoHandleHandle handle )
+{
+    size_t              payload_offset = SCOREP_Allocator_RoundupToAlignment( sizeof( SCOREP_IoHandleDef ) );
+    SCOREP_IoHandleDef* def            = SCOREP_LOCAL_HANDLE_DEREF( handle, IoHandle );
 
-SCOREP_IoHandleHandle
-SCOREP_IoHandleHandle_GetParentHandle( SCOREP_IoHandleHandle handle );
+    return ( char* )def + payload_offset;
+}
 
-SCOREP_IoParadigmType
-SCOREP_IoHandleHandle_GetIoParadigm( SCOREP_IoHandleHandle handle );
+static inline SCOREP_IoHandleHandle
+SCOREP_IoHandleHandle_GetParentHandle( SCOREP_IoHandleHandle handle )
+{
+    return SCOREP_LOCAL_HANDLE_DEREF( handle, IoHandle )->parent_handle;
+}
+
+static inline SCOREP_IoHandleHandle
+SCOREP_IoHandleHandle_GetIoParadigm( SCOREP_IoHandleHandle handle )
+{
+    return SCOREP_LOCAL_HANDLE_DEREF( handle, IoHandle )->io_paradigm_type;
+}
 
 void
 scorep_definitions_unify_io_handle( SCOREP_IoHandleDef*                  definition,

src/measurement/definitions/SCOREP_Definitions.c
@@ -305,15 +305,3 @@ SCOREP_Definitions_GetNumberOfUnifiedCallpathDefinitions( void )
 {
     return scorep_unified_definition_manager->callpath.counter;
 }
-
-
-uint32_t
-SCOREP_Definitions_HandleToId( SCOREP_AnyHandle handle )
-{
-    if ( handle == SCOREP_MOVABLE_NULL )
-    {
-        return UINT32_MAX;
-    }
-
-    return SCOREP_LOCAL_HANDLE_TO_ID( handle, Any );
-}

src/measurement/definitions/include/SCOREP_Definitions.h
@@ -300,36 +300,6 @@ scorep_definitions_manager_entry_free_mapping( scorep_definitions_manager_entry*
 }
 
 
-#include "scorep_definitions_string.h"
-#include "scorep_definitions_source_file.h"
-#include "scorep_definitions_location_group.h"
-#include "scorep_definitions_location.h"
-#include "scorep_definitions_system_tree_node.h"
-#include "scorep_definitions_system_tree_node_property.h"
-#include "scorep_definitions_region.h"
-#include "scorep_definitions_communicator.h"
-#include "scorep_definitions_group.h"
-#include "scorep_definitions_metric.h"
-#include "scorep_definitions_sampling_set.h"
-#include "scorep_definitions_sampling_set_recorder.h"
-#include "scorep_definitions_paradigm.h"
-#include "scorep_definitions_parameter.h"
-#include "scorep_definitions_callpath.h"
-#include "scorep_definitions_clock_offset.h"
-#include "scorep_definitions_property.h"
-#include "scorep_definitions_rma_window.h"
-#include "scorep_definitions_topology.h"
-#include "scorep_definitions_io_handle.h"
-#include "scorep_definitions_io_file.h"
-#include "scorep_definitions_io_file_property.h"
-#include "scorep_definitions_io_paradigm.h"
-#include "scorep_definitions_marker_group.h"
-#include "scorep_definitions_marker.h"
-#include "scorep_definitions_attribute.h"
-#include "scorep_definitions_location_property.h"
-#include "scorep_definitions_source_code_location.h"
-#include "scorep_definitions_calling_context.h"
-
 /* super object for all definitions */
 SCOREP_DEFINE_DEFINITION_TYPE( Any )
 {
@@ -340,8 +310,16 @@ SCOREP_DEFINE_DEFINITION_TYPE( Any )
 /* Return the unique id associated with @a handle.
  * So far only used in UTILS_DEBUG* statements, no need to introduce
  * dependency to SCOREP_Memory by inlining it.  */
-uint32_t
-SCOREP_Definitions_HandleToId( SCOREP_AnyHandle handle );
+static inline uint32_t
+SCOREP_Definitions_HandleToId( SCOREP_AnyHandle handle )
+{
+    if ( handle == SCOREP_MOVABLE_NULL )
+    {
+        return UINT32_MAX;
+    }
+
+    return SCOREP_LOCAL_HANDLE_TO_ID( handle, Any );
+}
 
 
 // the list of definitions for what we generate mappings to global
@@ -452,6 +430,37 @@ SCOREP_Definitions_InitializeDefinitionManager(
  */
 uint32_t
 SCOREP_Definitions_GetNumberOfUnifiedCallpathDefinitions( void );
+
+
+#include "scorep_definitions_string.h"
+#include "scorep_definitions_source_file.h"
+#include "scorep_definitions_location_group.h"
+#include "scorep_definitions_location.h"
+#include "scorep_definitions_system_tree_node.h"
+#include "scorep_definitions_system_tree_node_property.h"
+#include "scorep_definitions_region.h"
+#include "scorep_definitions_communicator.h"
+#include "scorep_definitions_group.h"
+#include "scorep_definitions_metric.h"
+#include "scorep_definitions_sampling_set.h"
+#include "scorep_definitions_sampling_set_recorder.h"
+#include "scorep_definitions_paradigm.h"
+#include "scorep_definitions_parameter.h"
+#include "scorep_definitions_callpath.h"
+#include "scorep_definitions_clock_offset.h"
+#include "scorep_definitions_property.h"
+#include "scorep_definitions_rma_window.h"
+#include "scorep_definitions_topology.h"
+#include "scorep_definitions_io_handle.h"
+#include "scorep_definitions_io_file.h"
+#include "scorep_definitions_io_file_property.h"
+#include "scorep_definitions_io_paradigm.h"
+#include "scorep_definitions_marker_group.h"
+#include "scorep_definitions_marker.h"
+#include "scorep_definitions_attribute.h"
+#include "scorep_definitions_location_property.h"
+#include "scorep_definitions_source_code_location.h"
+#include "scorep_definitions_calling_context.h"
 
 /*@}*/
 

src/measurement/definitions/include/scorep_definitions_attribute.h
@@ -70,12 +70,18 @@ SCOREP_Definitions_NewAttribute( const char*          name,
                                  const char*          description,
                                  SCOREP_AttributeType type );
 
-uint32_t
-SCOREP_AttributeHandle_GetId( SCOREP_AttributeHandle handle );
+static inline uint32_t
+SCOREP_AttributeHandle_GetId( SCOREP_AttributeHandle handle )
+{
+    return SCOREP_LOCAL_HANDLE_TO_ID( handle, Attribute );
+}
 
-
-SCOREP_AttributeType
-SCOREP_AttributeHandle_GetType( SCOREP_AttributeHandle handle );
+static inline SCOREP_AttributeType
+SCOREP_AttributeHandle_GetType( SCOREP_AttributeHandle handle )
+{
+    return SCOREP_HANDLE_DEREF( handle, Attribute,
+                                SCOREP_Memory_GetLocalDefinitionPageManager() )->type;
+}
 
 void
 scorep_definitions_unify_attribute( SCOREP_AttributeDef*                 definition,

src/measurement/definitions/include/scorep_definitions_calling_context.h
@@ -46,11 +46,19 @@ SCOREP_Definitions_NewCallingContext( uint64_t                        ip,
                                       SCOREP_SourceCodeLocationHandle scl,
                                       SCOREP_CallingContextHandle     parent );
 
-SCOREP_RegionHandle
-SCOREP_CallingContextHandle_GetRegion( SCOREP_CallingContextHandle handle );
+static inline SCOREP_RegionHandle
+SCOREP_CallingContextHandle_GetRegion( SCOREP_CallingContextHandle handle )
+{
+    SCOREP_CallingContextDef* calling_context = SCOREP_LOCAL_HANDLE_DEREF( handle, CallingContext );
+    return calling_context->region_handle;
+}
 
-SCOREP_CallingContextHandle
-SCOREP_CallingContextHandle_GetParent( SCOREP_CallingContextHandle handle );
+static inline SCOREP_CallingContextHandle
+SCOREP_CallingContextHandle_GetParent( SCOREP_CallingContextHandle handle )
+{
+    SCOREP_CallingContextDef* calling_context = SCOREP_LOCAL_HANDLE_DEREF( handle, CallingContext );
+    return calling_context->parent_handle;
+}
 
 void
 scorep_definitions_unify_calling_context( SCOREP_CallingContextDef*            definition,

src/measurement/definitions/include/scorep_definitions_callpath.h
@@ -81,8 +81,12 @@ scorep_definitions_unify_callpath( SCOREP_CallpathDef*                  definiti
  * the mappings.
  * @param handle handle to local callpath handle.
  */
-uint32_t
-SCOREP_CallpathHandle_GetUnifiedId( SCOREP_CallpathHandle handle );
+static inline uint32_t
+SCOREP_CallpathHandle_GetUnifiedId( SCOREP_CallpathHandle handle )
+{
+    uint32_t local_id = SCOREP_LOCAL_HANDLE_TO_ID( handle, Callpath );
+    return scorep_local_definition_manager.callpath.mapping[ local_id ];
+}
 
 
 /**

src/measurement/definitions/include/scorep_definitions_communicator.h
@@ -159,15 +159,24 @@ SCOREP_Definitions_NewInterimCommunicatorCustom(
     ... );
 
 
-SCOREP_InterimCommunicatorHandle
-SCOREP_InterimCommunicatorHandle_GetParent( SCOREP_InterimCommunicatorHandle commHandle );
+static inline SCOREP_InterimCommunicatorHandle
+SCOREP_InterimCommunicatorHandle_GetParent( SCOREP_InterimCommunicatorHandle commHandle )
+{
+    return SCOREP_LOCAL_HANDLE_DEREF( commHandle, InterimCommunicator )->parent_handle;
+}
 
 
 /**
  * Get access to the payload from a communicator definition.
  */
-void*
-SCOREP_InterimCommunicatorHandle_GetPayload( SCOREP_InterimCommunicatorHandle handle );
+static inline void*
+SCOREP_InterimCommunicatorHandle_GetPayload( SCOREP_InterimCommunicatorHandle handle )
+{
+    return ( char* )SCOREP_LOCAL_HANDLE_DEREF( handle, InterimCommunicator )
+           + SCOREP_Allocator_RoundupToAlignment( sizeof( SCOREP_InterimCommunicatorDef ) );
+}
+
+
 
 
 /**

src/measurement/definitions/include/scorep_definitions_metric.h
@@ -95,8 +95,12 @@ SCOREP_Definitions_GetNumberOfUnifiedMetricDefinitions( void );
  * the mappings.
  * @param handle handle to local metric handle.
  */
-uint32_t
-SCOREP_MetricHandle_GetUnifiedId( SCOREP_MetricHandle handle );
+static inline uint32_t
+SCOREP_MetricHandle_GetUnifiedId( SCOREP_MetricHandle handle )
+{
+    uint32_t local_id = SCOREP_LOCAL_HANDLE_TO_ID( handle, Metric );
+    return scorep_local_definition_manager.metric.mapping[ local_id ];
+}
 
 
 /**
@@ -108,10 +112,13 @@ SCOREP_MetricHandle_GetUnified( SCOREP_MetricHandle handle );
 
 /**
  * Returns the value type of a metric.
- * @param handle to local metric definition.
+ * @param handle to local meric definition.
  */
-SCOREP_MetricValueType
-SCOREP_MetricHandle_GetValueType( SCOREP_MetricHandle handle );
+static inline SCOREP_MetricValueType
+SCOREP_MetricHandle_GetValueType( SCOREP_MetricHandle handle )
+{
+    return SCOREP_LOCAL_HANDLE_DEREF( handle, Metric )->value_type;
+}
 
 
 /**
@@ -128,14 +135,6 @@ SCOREP_MetricHandle_GetName( SCOREP_MetricHandle handle );
  */
 SCOREP_MetricProfilingType
 SCOREP_MetricHandle_GetProfilingType( SCOREP_MetricHandle handle );
-
-
-/**
- * Returns the parent of a metric.
- * @param handle to local metric definition.
- */
-SCOREP_MetricHandle
-SCOREP_MetricHandle_GetParent( SCOREP_MetricHandle handle );
 
 
 /**

commit 86f137c6640027c8f706caf1fcc05aa087bc8623

Fix wrong return type introduced in 9c70d3e129
No warnings with gcc, but ancient pgcc aborts.

src/measurement/definitions/include/scorep_definitions_io_handle.h
@@ -113,7 +113,7 @@ SCOREP_IoHandleHandle_GetParentHandle( SCOREP_IoHandleHandle handle )
     return SCOREP_LOCAL_HANDLE_DEREF( handle, IoHandle )->parent_handle;
 }
 
-static inline SCOREP_IoHandleHandle
+static inline SCOREP_IoParadigmType
 SCOREP_IoHandleHandle_GetIoParadigm( SCOREP_IoHandleHandle handle )
 {
     return SCOREP_LOCAL_HANDLE_DEREF( handle, IoHandle )->io_paradigm_type;