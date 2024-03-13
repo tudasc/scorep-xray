5106a0b56bc22b7189709b2384609e6874a32319
Jan André Reuter <j.reuter@fz-juelich.de>
2024-03-13 14:04:54 +0100

    [OMPT] Ensure OMPT init. on device init callback
    Beginning with LLVM 18, the runtime will initialize all visible devices
    on program start. Fortunately, this is done at a point where we are able
    to initialize Score-P. If we don't initialize it for device init. as
    well, program execution of a program containing OpenMP target regions and a
    CUDA adapter enabled will probably abort. Therefore, also add the ensure
    initialized macro for `ompt_callback_device_initialize`.
    
    Signed-off-by: Jan André Reuter <j.reuter@fz-juelich.de>

--- src/adapters/ompt/scorep_ompt_events_device.inc.c
+++ src/adapters/ompt/scorep_ompt_events_device.inc.c
@@ -1,6 +1,8 @@
 /*
  * This file is part of the Score-P software (http://www.score-p.org)
  *
+ * Copyright (c) 2022, 2024,
+ * Forschungszentrum Juelich GmbH, Germany
  *
  * This software may be modified and distributed under the terms of
  * a BSD-style license. See the COPYING file in the package base
@@ -25,5 +27,6 @@ scorep_ompt_cb_device_initialize( int                    device_num,
     UTILS_DEBUG( "[%s] atid %" PRIu32 " | device_num %d | type %s | lookup %p | "
                  "documentation %s", UTILS_FUNCTION_NAME, adapter_tid, device_num,
                  type, lookup, documentation );
+    SCOREP_OMPT_ENSURE_INITIALIZED();
     SCOREP_IN_MEASUREMENT_DECREMENT();
 }
